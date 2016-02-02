066ed98320226cf378874c89e401df5a98c5e2e7
tochman <thomas@nocebo.se>
2016-02-02 13:24:32 +0100

    cleaned up and refactored library and person

--- lib/library.rb
+++ lib/library.rb
@@ -1,46 +1,49 @@
-require 'book'
 require 'date'
 
 class Library
-   attr_accessor :books   #TODO duedate person status
+  attr_accessor :books #TODO duedate person status
 
-   def initialize(library)
-    @books = []
-   end
-
-   def check_in(book)
-    add = {item: book, status: :okay, person: :non, duedate: " " }
-    @books << add
-   end
-
-   def lend(title)
-    case
-     when books.detect{|a| a[:title] == title}[:status] = :lended
-       then 'Sorry, book is already with another reader'
-    when check = portfolio.select{|a| a[:duedate] > Date.today.strftime('%F')}
-      if check != []
-       then 'Sorry, you have one overdue book to return'
-    else
-        items = books.detect{|a| a[:title] == title}
-        items[:status] = :lended
-        items[:person] = :MEEE
-        items[:dudate] = duedate
-
-        #TODO: command to replace the original hash / or delete it
-    end
+  def initialize(options={})
+    @books = options[:books] || []
   end
 
- def return(title)
-        items = books.detect{|a| a[:title] == title}
-        items[:status] = :okay
-        items[:person] = :non
-        items[:dudate] = duedate
+  def check_in(book)
+    add = {title: book[:title],
+           author: book[:author],
+           status: :okay,
+           person: :non,
+           duedate: " "}
+    @books.push add
+  end
 
-        #TODO: command to replace the original hash / or delete it
- end
+  def lend(title, user)
+    book = @books.detect { |a| a[:title] == title[:title] }
+    case
+      when book[:status] == :lended then
+        'Sorry, book is already with another reader'
+      when user.portfolio.select { |a| a[:duedate] > Date.today.strftime('%F') } != [] then
+        'Sorry, you have one overdue book to return'
+      else
+        book[:status] = :lended
+        book[:person] = :MEEE
+        book[:duedate] = set_duedate
+    end
+    book
+  end
 
- def duedate
+
+  def return(title)
+    items = books.detect { |a| a[:title] == title }
+    items[:status] = :okay
+    items[:person] = :non
+    items[:duedate] = duedate
+
+    #TODO: command to replace the original hash / or delete it
+  end
+
+  def set_duedate
     Date.today.next_month.strftime('%F')
- end
+  end
+
 end
-end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,31 @@
-require 'book'
 require 'date'
+require 'pry'
 
 class Person
- attr_accessor :portfolio
+  attr_accessor :portfolio
 
-  def initialize(person)
-   @portfolio = []
+  def initialize
+    @portfolio = []
   end
 
-def rent(book)
-    items = books.detect{|a| a[:title] == title}
-    add = {item: items, duedate: duedate }
-    @portfolio.push add
+  def rent(library, args={})
+    item = library.books.detect{|obj| obj[args.keys.first] == (args[args.keys.first]).to_s }
+    #item = library.books.detect { |a| a[:title] == book[:title] }
+    to_add = {}
+    if item
+      to_add.merge!({title: item[:title],
+                    author: item[:author],
+                    duedate: duedate })
+      @portfolio.push to_add
+    else
+      'Nothing was added'
+    end
+
+  end
+
+
+  def duedate
+    "2016-03-02"
+  end
 
 end
