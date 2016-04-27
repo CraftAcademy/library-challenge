76a7813eb71404fe3e03b292c16e256e5393944b
Yan Zhang <emma2740@gmail.com>
2016-04-27 15:23:48 +0200

    Add Person class and its test class. Add the first test.

---
+++ spec/person_spec.rb
@@ -0,0 +1,22 @@
+require './lib/person.rb'
+
+describe Person do
+  let(:library) { instance_double('Library',
+    book_list:[{title:'Veg Recipes', author:'Tan Li'},
+                {title:'Thinking in C++', author:'Bruce Eckel'}])}
+
+# As an individual
+# In order to get my hands on a good book
+# I would like to see a list of books currently available in the library
+# with information about the title and author
+  it 'is expected to see a list of books currently available in the library' do
+    expect(library.book_list).not_to be_empty
+  end
+
+
+
+# As an individual
+# In order to avoid awkward moments at the library
+# I would like to know when my book is supposed to be returned
+
+end
