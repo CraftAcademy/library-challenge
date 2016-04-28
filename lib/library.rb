18abe2a6c74323481e4ca3930fe1daa5cddadd48
Yan Zhang <emma2740@gmail.com>
2016-04-28 12:06:31 +0200

    Test to checkout one book

--- lib/library.rb
+++ lib/library.rb
@@ -1,11 +1,14 @@
 class Library
   attr_reader :book_list
-  
+
   def initialize
     @book_list = [{title: 'Steve Jobs', author:'Walter Isaacsson'},
                 {title:'Veg Recipes', author:'Tan Li'},
                 {title:'Thinking in C++', author:'Bruce Eckel'}]
+  end
 
+  def checkout(book)
+    @book_list.delete(book)
   end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,10 +9,15 @@ describe Library do
     expect(subject.book_list).not_to be_empty
   end
 
-
   # As a library
   # In order to have good books to offer to the public
   # I would like to be able to allow individuals to check out a book
+
+  it 'Reduce books from its list for check out' do
+    subject.checkout( {title: 'Steve Jobs', author:'Walter Isaacsson'})
+    expect(subject.book_list).to include({title:'Veg Recipes', author:'Tan Li'},
+                                {title:'Thinking in C++', author:'Bruce Eckel'})
+  end
 
 
   # As a library
