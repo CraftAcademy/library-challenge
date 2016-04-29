4d7beb9d3e96e623c5711e71ceaaed8c61c430c8
tochman <thomas@nocebo.se>
2016-04-29 12:31:12 +0200

    adds a return date to checkedout book

--- lib/library.rb
+++ lib/library.rb
@@ -1,5 +1,6 @@
 class Library
   attr_accessor :catalog
+  DEFAULT_CHECKOUT_PERIOD = 1
 
   def initialize
     @catalog = YAML.load_file('./lib/catalog.yml')
@@ -7,6 +8,7 @@ class Library
 
   def checkout(book)
     book[:available] = false
+    book[:return_date] = Date.today.next_month(DEFAULT_CHECKOUT_PERIOD)
   end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,30 +2,40 @@ require './lib/library'
 
 describe Library do
 
+  let(:book) do
+    {item:
+         {title: 'Skratta lagom! Sa pappa Åberg',
+          author: 'Gunilla Bergström'},
+     available: true,
+     return_date: nil}
+  end
+
   it 'is expected to have a :catalog' do
     expect(subject.catalog).not_to be nil
   end
 
   it ':catalog is expected to have items' do
-    expected_content = {item:
-                            {title: 'Skratta lagom! Sa pappa Åberg',
-                             author: 'Gunilla Bergström'},
-                        available: true,
-                        return_date: nil}
-    expect(subject.catalog).to include expected_content
+    expect(subject.catalog).to include book
   end
 
   it ':catalog has 5 items' do
     expect(subject.catalog.count).to eq 5
   end
 
-  it 'allows book for checkout' do
-    book = {item:
-                            {title: 'Skratta lagom! Sa pappa Åberg',
-                             author: 'Gunilla Bergström'},
-                        available: true,
-                        return_date: nil}
-    subject.checkout(book)
-    expect(book[:available]).to eq false
+  describe '#checkout is expected' do
+
+    before do
+      subject.checkout(book)
+    end
+
+    it 'to set :avaiable to false' do
+      expect(book[:available]).to eq false
+    end
+
+    it 'to set a return date' do
+      expect(book[:return_date]).to eq Date.today.next_month
+    end
+
   end
+
 end
