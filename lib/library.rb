edef154d85646aa934f9b4a78d97d5a80c96e659
MikaelFeher <mikael.feher@gmail.com>
2016-06-18 18:48:33 +0200

    added method to search for title

--- lib/library.rb
+++ lib/library.rb
@@ -3,15 +3,24 @@ require 'date'
 
 class Library
 
-  attr_accessor :collection, :available_books
+  attr_accessor :collection, :available_books, :checked_out_books, :title
 
   def initialize
     @collection = YAML.load_file('./lib/data.yml')
     @available_books = check_for_available_books
+    @checked_out_books = unavailable?
+    @title = title
   end
 
   def check_for_available_books
     @collection.select { |item| item[:available] == true }
   end
 
+  def unavailable?
+    @collection.select { |item| item[:available] == false }
+  end
+
+  def search_title(title)
+    title_result = @collection.select { |obj| obj[:item][:title].include? title.to_s  }
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -10,10 +10,22 @@ describe Library do
     expect(subject.collection).to eq expected_collection
   end
 
-  it 'checks for books available for checkout' do
+  it 'checks if there are books available for checkout' do
     expect(subject.available_books).not_to be_empty
   end
 
+  it 'are there any books checked out' do
+    expect(subject.checked_out_books.empty?).to eq false
+  end
+
+  it 'can search for a specific title' do
+      result = subject.search_title('Skratta lagom! Sa pappa Åberg')
+      expect(subject.search_title('Skratta lagom! Sa pappa Åberg')).to eq result
+  end
+
+  # it 'does checking a book out remove it from the available list' do
+  #   expect
+  # end
 
   # it 'checked out books recieve a return date' do
   #   expected_return_date = Date.today.next_month
