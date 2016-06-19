08f62e445fb2566b4d385c490ec21c30cbdba054
MikaelFeher <mikael.feher@gmail.com>
2016-06-19 15:00:51 +0200

    added test for the title search method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -14,33 +14,12 @@ describe Library do
     expect(subject.list_available_books).not_to be_empty
   end
 
-  it 'expects to find desired title' do
-    desired_title = subject.find_title('Alfons och soldatpappan')
-    expect(subject.find_title(title)).to eq desired_title
+  it 'can search for a specific title' do
+      result =  subject.find_title("Skratta lagom! Sa pappa Åberg")
+      expected_item = {item: {title: "Skratta lagom! Sa pappa Åberg",
+                      author: "Gunilla Bergström"}, available: false,
+                      return_date: "2016-05-25"}
+      expect(result).to include expected_item
   end
 
-  # it 'are there any books checked out' do
-  #   expect(subject.checked_out_books.empty?).to eq false
-  # end
-  #
-  # it 'can search for a specific title' do
-  #     result =  subject.search_title("Skratta lagom! Sa pappa Åberg")
-  #     expected_item = {item: {title: "Skratta lagom! Sa pappa Åberg"}}
-  #     expect(result).to include expected_item
-  # end
-
-  # it 'does checking a book out remove it from the available list' do
-  #   expect
-  # end
-
-  # it 'checked out books recieve a return date' do
-  #   expected_return_date = Date.today.next_month
-  #   expect(library.set_return_date).to eq expected_return_date
-  # end
-  #
-
-  #
-  # it 'books can be checked out' do
-  #   expect(library.check_out)         # will come back to this later.
-  # end
 end
