5afc59cbd1b20cd7f12f173f2e52011643b580ac
Lucia De Mojà <luciademoja@gmail.com>
2016-07-18 21:53:25 +0200

    test for checking if a book is available missing implementation

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,4 +5,8 @@ describe Library do
   it "Has a not empty collection of books" do
     expect(:books).not_to be_empty
   end
+
+  it "Checks that a book is available" do
+    expect(subject.is_available?("Alfons och soldatpappan", "Gunilla Bergström")).to be true
+  end
 end
