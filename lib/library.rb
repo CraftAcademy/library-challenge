1cd8433816ba699951058b53c83b412d16170082
Lucia De Mojà <luciademoja@gmail.com>
2016-07-18 22:47:19 +0200

    book_checkout method and private method to get_book, refactoring is_available method and allowing individuals to check out a book

--- lib/library.rb
+++ lib/library.rb
@@ -8,11 +8,27 @@ class Library
   end
 
   def is_available?(title, author)
-    book_id = books.detect { |obj| obj[:item][:title] == title and obj[:item][:author] == author }
+    book_id = get_book(title, author)
       if book_id == nil
         false
       else
         book_id[:available]
       end
+  end
+
+  def book_checkout(title, author)
+    if is_available?(title, author)
+      my_book = get_book(title, author)
+      my_book[:available] = false
+      { title: title, author: author, message: "item booked successfully" }
+    else
+      raise "This book is not available"
+    end
+  end
+
+  private
+
+  def get_book(title, author)
+    books.detect { |obj| obj[:item][:title] == title and obj[:item][:author] == author }
   end
 end
