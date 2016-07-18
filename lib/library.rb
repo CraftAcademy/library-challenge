a25d64672a3eb2f0237beedd93ee23b091743c75
Lucia De Mojà <luciademoja@gmail.com>
2016-07-18 22:18:41 +0200

    implemented is_available? method in library to check availability for a specific book

--- lib/library.rb
+++ lib/library.rb
@@ -4,6 +4,15 @@ class Library
   attr_accessor :books
 
   def initialize
-    @books = YAML.load_file('./lib/data.yml') 
+    @books = YAML.load_file('./lib/data.yml')
+  end
+
+  def is_available?(title, author)
+    book_id = books.detect { |obj| obj[:item][:title] == title and obj[:item][:author] == author }
+      if book_id == nil
+        false
+      else
+        book_id[:available]
+      end
   end
 end
