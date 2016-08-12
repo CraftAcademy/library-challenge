3f05160b31bce2a5ab2d9cbfb97bd601cf073431
thesuss <lullisuss@gmail.com>
2016-08-12 10:42:00 +0200

    all person tests are passing

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,8 +6,17 @@ describe Person do
     expect(subject.list).not_to be nil
   end
 
+  it 'has title' do
+    expect(subject.list[0][:item][:title]).not_to be nil
+  end
+
+  it 'has author' do
+    expect(subject.list[0][:item][:author]).not_to be nil
+  end
+
   it 'knows due date' do
     expect(subject.list[0][:return_date]).not_to be nil
   end
+
 
 end
