968edcfc23647b3698143642eb94a22efa567934
thesuss <lullisuss@gmail.com>
2016-08-12 10:30:34 +0200

    Library has a list

--- lib/library.rb
+++ lib/library.rb
@@ -4,14 +4,8 @@ require "Date"
 class Library
   attr_accessor :items
 
-  def initialize(items)
-    self.items = items
+  def initialize
+    @items = YAML.load_file('./lib/library_list.yml')
   end
 
-  def has_item?(search_item)
-    for item in items
-      return true if item == search_item
-    end
-  false
-  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,6 +1,12 @@
 require './lib/library.rb'
 
-describe Checkout do
+describe Library do
   let(:person) {double('Person')}
-  
+
+  it 'is expected to have a list' do
+    expect(subject.items).not_to be nil
+  end
+
+
+
 end
