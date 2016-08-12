10be6601a18140211af802e75e7fb7b1921ce701
thesuss <lullisuss@gmail.com>
2016-08-12 11:44:13 +0200

    Library sets a return-date

--- lib/library.rb
+++ lib/library.rb
@@ -8,4 +8,10 @@ class Library
     @items = YAML.load_file('./lib/library_list.yml')
   end
 
+  def set_return_date(item_number)
+    items[item_number][:return_date] = "2022-12-12"
+    File.open('./lib/library_list.yml', 'w') { |f| f.write items.to_yaml }
+    items[item_number][:return_date]
+  end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,10 +3,13 @@ require './lib/library.rb'
 describe Library do
   let(:person) {double('Person')}
 
-  it 'is expected to have a list' do
+  it 'has a list of items' do
     expect(subject.items).not_to be nil
   end
 
-
+  it 'sets return_date' do
+    subject.set_return_date(0)
+    expect(subject.items[0][:return_date]).to eq(subject.set_return_date(0))
+  end
 
 end
