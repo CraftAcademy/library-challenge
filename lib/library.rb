33a5304ec03f98c2feb88713a45905d5394c9a81
thesuss <lullisuss@gmail.com>
2016-08-12 11:54:39 +0200

    Library sets a return-date typo fix

--- lib/library.rb
+++ lib/library.rb
@@ -4,14 +4,23 @@ require "Date"
 class Library
   attr_accessor :items
 
+   ITEM_BORROW_TIME = 30
+
   def initialize
     @items = YAML.load_file('./lib/library_list.yml')
   end
 
   def set_return_date(item_number)
-    items[item_number][:return_date] = "2022-12-12"
+    items[item_number][:return_date] = due_date
     File.open('./lib/library_list.yml', 'w') { |f| f.write items.to_yaml }
     items[item_number][:return_date]
+  end
+
+  private
+
+  def due_date
+    #lägger till 30 till dagens datum
+    Date.today.next_day(ITEM_BORROW_TIME)
   end
 
 end
