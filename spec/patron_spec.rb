1ae23c1a071a443ba30f184473ea92fa4344ae83
Raoul DIFFOUO <diraulo@gmail.com>
2016-11-18 16:42:42 +0200

    update patron spec

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,4 +2,9 @@ describe 'Patron' do
   it 'should have a name' do
     expect(subject).to respond_to(:name)
   end
+
+  it 'does something' do
+    result = 1 + 1
+    expect(result).to eq 2
+  end
 end
