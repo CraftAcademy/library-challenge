3baf18bdadafdcbeee5be13a093f6674efd31054
Philip <Philip.zdb@gmail.com>
2016-11-18 19:26:17 +0100

    Trying to make library.search work

--- lib/library.rb
+++ lib/library.rb
@@ -6,4 +6,17 @@ class Library
   def initialize
     @books = YAML.load_file('./lib/data.yml')
   end
+
+  def search(book)
+    @books.select{ |obj| obj[:item][:title].include? book }
+  end
 end
+#   def available
+#     puts @books.select{ |obj| obj[:available].include?  }
+#   end
+# end
+
+  # if book == nil
+  #   @books = book
+  #   else
+  #  end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,4 +6,14 @@ describe Library do
   it 'should have books available' do
     expect(subject.books).not_to be nil
   end
+
+  # it 'can check which books are available' do
+  #   expected_output = { }
+  #   expect(subject.available(nil)).to eq expected_output
+  # end
+
+  it 'can search for a specific book' do
+    expected_output = [{:item=>{:title=>"Pippi Långstrump", :author=>"Astrid Lindgren"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Pippi Långstrump går ombord", :author=>"Astrid Lindgren"}, :available=>true, :return_date=>nil}]
+    expect(subject.search('pippi')).to eq expected_output
+  end
 end
