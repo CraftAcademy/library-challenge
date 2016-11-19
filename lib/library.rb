6c6bb882823a4c2ebbdc9827cf69800c09d36100
Philip <Philip.zdb@gmail.com>
2016-11-19 16:39:44 +0100

    Added so you when you borrow a book, available is set to false

--- lib/library.rb
+++ lib/library.rb
@@ -8,10 +8,21 @@ class Library
   end
 
   def search(book)
-    @books.select{ |obj| obj[:item][:title].include? book }
+    search_book = @books.select{ |obj| obj[:item][:title].include? book }
   end
 
   def what_is_available?
     @books.select { |obj| obj[:available] == true }
   end
+
+  def borrow(book)
+    # if search(book).map{|obj| obj[:available] = false}
+    #   raise 'book is not availble right now'
+    # else
+    search(book).map{|obj| obj[:available] = false}
+    File.open('./lib/data.yml', 'w') { |f| f.write @books.to_yaml }
+  end
+
+  # def return(book)
+  #   gör om filen till book :available false
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,6 +9,8 @@ describe Library do
 
 #change this to lambda??? to long maybe
   it 'can check which books are available' do
+    # command = lambda {subject.what_is_available?}
+    # expect(command.call).to be_truthy
     expected_output = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Osynligt med Alfons", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Pippi Långstrump", :author=>"Astrid Lindgren"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Pippi Långstrump går ombord", :author=>"Astrid Lindgren"}, :available=>true, :return_date=>nil}]
     expect(subject.what_is_available?).to eq expected_output
   end
@@ -17,4 +19,42 @@ describe Library do
     expected_output = [{:item=>{:title=>"Pippi Långstrump", :author=>"Astrid Lindgren"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Pippi Långstrump går ombord", :author=>"Astrid Lindgren"}, :available=>true, :return_date=>nil}]
     expect(subject.search('Pippi')).to eq expected_output
   end
+
+  describe 'be able to lend a book' do
+    # let(:person) { instance_double('Person'}
+    before {
+      subject.search('Skratta lagom! Sa pappa Åberg').map{|obj| obj[:available] = true
+      File.open('./lib/data.yml', 'w') { |f| f.write @books.to_yaml }}
+    }
+
+    # before do
+    #   allow(person).to receive(:borrowed_books).and_return({:item=>{:title=>"Pippi Långstrump går ombord", :author=>"Astrid Lindgren"}, :available=>false, :return_date=>nil})
+    # end
+
+
+    it 'Should set so available: false upon lending' do
+      subject.borrow('Skratta lagom! Sa pappa Åberg')
+      expected_output = [{:item=>{:title=>'Skratta lagom! Sa pappa Åberg', :author=>"Gunilla Bergström"}, :available=>false, :return_date=>nil}]
+      expect(subject.search('Skratta lagom! Sa pappa Åberg')).to eq expected_output
+    end
+  end
+
+    # it 'should set a return_date on that book' do
+      # philip.borrow('Pippi Långstrump går ombord')
+      # expected_output = [{:item=>{:title=>"Pippi Långstrump", :author=>"Astrid Lindgren"}, :available=>false, :return_date=>nil}
+    # end
+    #
+    # it 'should set so current owner of that book is person' do
+    #
+    # end
+
+    # it 'should give an error if book is not available' do
+    #
+    # end
+    #
+    #
+    # it 'A person should be able to return a book' do
+    #    == true
+    # end
+  # end
 end
