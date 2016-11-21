4390d399bb61a4556d708cbd44759d211372b08a
Philip <Philip.zdb@gmail.com>
2016-11-21 08:00:25 +0100

    Individual can now borrow a book, borrowed_books is updated with title and return date

--- lib/individual.rb
+++ lib/individual.rb
@@ -7,19 +7,20 @@ class Individual
     @borrowed_books = YAML.load_file('./lib/borrowed_books.yml')
     @name = 'Philip Testing'
   end
-#
-#   def borrow(book_title, (library))
-#     if @name == nil
-#       raise 'You need a name in order to borrow a book'
-#     else
-#     (library).borrow(book_title, self)
-#     @borrowed_books.map do |obj|
-#       obj[:title] = (book_title)
-#       obj[:return_date] = Date.today.next_month(1).strftime('%d/%m/%y')
-#     end
-#     File.open('./lib/borrowed_books.yml', 'w') { |f| f.write @borrowed_books.to_yaml }
-#   end
-# end
+
+  def borrow(book_title, (library))
+    if @name == nil
+      raise 'You need a name in order to borrow a book'
+    else
+    (library).borrow(book_title, self)
+    @borrowed_books.map do |obj|
+      obj[:title] = (book_title)
+      obj[:return_date] = Date.today.next_month(1).strftime('%d/%m/%y')
+    end
+    File.open('./lib/borrowed_books.yml', 'w') { |f| f.write @borrowed_books.to_yaml }
+  end
+end
+end
 #
 #   def return(book_title, (library))
 #     (library).return(book_title)
--- spec/individual_spec.rb
+++ spec/individual_spec.rb
@@ -14,25 +14,26 @@ describe Individual do
     expected_output = [{:title=>nil, :return_date=>nil}]
     expect(subject.borrowed_books).to eq expected_output
   end
-# 
-#   describe 'should be able to borrow a book' do
-#
-#     it 'individual requires a name in order to borrow a book' do
-#       subject.name = nil
-#       subject.borrow('Pippi Långstrump går ombord', library).to raise_error 'You need a name in order to borrow a book'
-#     end
-#
-#     it 'borrowed_books should be updated with reutrn date and title' do
-#       subject.borrow('Pippi Långstrump går ombord', library)
-#       expected_output = [{:title=>"Pippi Långstrump går ombord", :return_date=>"21/12/16"}]
-#       expect(subject.borrowed_books).to eq expected_output
-#     end
-#
-#     describe 'should be able to return a book'
-#       it 'if returned, book should be gone from individuals possession(borrowed_books)' do
-#       expected_output = [{:title=>nil, :return_date=>nil}]
-#       subject.return('Pippi Långstrump går ombord', library)
-#       expect(subject.borrowed_books).to eq expected_output
-#     end
-#   end
-# end
+
+  describe 'should be able to borrow a book' do
+
+    it 'individual requires a name in order to borrow a book' do
+      subject.name = nil
+      subject.borrow('Pippi Långstrump går ombord', library).to raise_error 'You need a name in order to borrow a book'
+    end
+
+    it 'borrowed_books should be updated with reutrn date and title' do
+      subject.borrow('Pippi Långstrump går ombord', library)
+      expected_output = [{:title=>"Pippi Långstrump går ombord", :return_date=>"21/12/16"}]
+      expect(subject.borrowed_books).to eq expected_output
+    end
+  end
+
+  #   describe 'should be able to return a book'
+  #     it 'if returned, book should be gone from individuals possession(borrowed_books)' do
+  #     expected_output = [{:title=>nil, :return_date=>nil}]
+  #     subject.return('Pippi Långstrump går ombord', library)
+  #     expect(subject.borrowed_books).to eq expected_output
+  #   end
+  # end
+end
