24fa86c20066586c8a2f0e9960ab4d60afa38a8a
blondabambi <ebba@aniansson.se>
2016-12-08 17:39:35 +0100

    make patron able to borrow books

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -9,4 +9,11 @@ describe Patron do
     expect(subject.borrowed_books).to eq expected_output
   end
 
+  it 'should be able to borrow books' do
+    subject.borrow('Pippi Långstrump')
+    expected_output = [{:item=>{:title=>'Pippi Långstrump', :author=>'Astrid Lindgren'}, :return_date=>Date.today.next_month(1).strftime('%d/%m')}]
+    expect(subject.borrowed_books).to eq expected_output
+  end
+
+
 end
