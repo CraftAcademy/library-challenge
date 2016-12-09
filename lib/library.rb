479ee0ba309631d8b77dedbd26c34398c059b5a4
kriizjones <kristoffer.s.karlsson@hotmail.com>
2016-12-09 14:47:22 +0100

    have done, find title, author and available books

--- lib/library.rb
+++ lib/library.rb
@@ -1,2 +1,23 @@
 class Library
+attr_accessor :collection
+
+  def initialize
+    @collection = YAML.load_file('./lib/data.yml')
+  end
+
+  def search_by_title(book_title)
+    @collection.detect { |book| book[:item][:title].include? book_title }
+  end
+
+  def search_by_author(book_author)
+    @collection.detect { |book| book[:item][:author].include? book_author }
+  end
+
+def available_books
+  available = collection.select { |book| book[:available] == true }
+
+  results = []
+  available.each { |book| results.push(book[:item][:title]) }
+  return results
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,72 @@
-require '/.lib/library.rb'
+require './lib/library.rb'
+
+describe Library do
+  it 'should have a collection of books' do
+    collection = YAML.load_file('./lib/data.yml')
+    expect(subject.collection).to eq collection
+  end
+
+  it 'should allow people to search for books by title' do
+    library = Library.new
+    book_title = 'Pippi Långstrump'
+    expected_book = {
+      item: {
+        title: 'Pippi Långstrump',
+        author: 'Astrid Lindgren'
+      },
+      available: true,
+      return_date: nil
+    }
+
+    actual_book = library.search_by_title(book_title)
+
+    expect(actual_book).to eq expected_book
+  end
+
+  it 'should allow people to search for books by title with different title' do
+    library = Library.new
+    book_title = 'Pappa Åberg åker utomlands'
+    expected_book = {
+      item: {
+        title: 'Pappa Åberg åker utomlands',
+        author: 'Gunilla Bergström'
+      },
+      available: true,
+      return_date: nil
+    }
+
+    actual_book = library.search_by_title(book_title)
+
+    expect(actual_book).to eq expected_book
+  end
+
+  it 'should allow people to search for books by author' do
+    library = Library.new
+    book_author = 'Astrid Lindgren'
+    expected_book = {
+      item: {
+        title: 'Pippi Långstrump',
+        author: 'Astrid Lindgren'
+      },
+      available: true,
+      return_date: nil
+    }
+
+    actual_book = library.search_by_author(book_author)
+
+    expect(actual_book).to eq expected_book
+  end
+
+  it 'should have books in for customers' do
+  library = Library.new
+  expected_books = [
+    'Alfons sover igen', 'Pappa Åberg åker utomlands', 'Osynligt med Alfons', 'Pippi Långstrump', 'Pippi Långstrump går ombord'
+  ]
+
+  actual_book = library.available_books
+
+  expect(actual_book).to eq expected_books
+end
+
+
+end
