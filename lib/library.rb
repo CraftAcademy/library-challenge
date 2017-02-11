d384185b6fb85eed4451d3e88264c8c308620bea
casperjureskog <lcasper@mac.com>
2017-02-12 00:46:00 +0100

    adds 4 test 3 solvd one to be fix

--- lib/library.rb
+++ lib/library.rb
@@ -2,20 +2,24 @@ require 'yaml'
 
 class Library
 
-  attr_accessor :books
+  attr_accessor
 
-  def initialize
+  def initialize(books)
     @books = YAML.load_file('./lib/data.yml')
+
   end
 
-
-
-  def books_available()
-
-    
+  def available_books
+    list = @books
+    list.select { |obj| obj[:available] == true  }
   end
 
+  def all_books
+    list = @books
+  end
 
-
-
+  def person_checkout
+    @books[0][:available] = false
+    @books[0][:return_date] = Date.today + 30
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,23 +1,29 @@
-#require 'yaml'
+require 'yaml'
 require './lib/library.rb'
 
 #collection = YAML.load_file('./lib/data.yml')
 describe Library do
 
+  subject { described_class.new(name: 'Casper') }
+
+  it 'list all books in library' do
+    expect(subject.all_books).not_to eq nil
+  end
+
   it 'list of books currently available in the library' do
-    expect(subject.books_available).not_to eq nil
+    expect(subject.available_books).not_to eq nil
   end
 
   it 'have a collection of books stored in a file' do
-    expect(collection).not_to eq nil
+    expect(YAML.load_file('./lib/data.yml')).not_to eq nil
   end
 
   it 'allow individuals to check out a book' do
-    expect(subject.person.checkout).to eq true
+    expect(subject.person_checkout).to eq true
   end
 
-  it 'set a return date on every check out, 1 month from checkout date' do
-    expect(subject.checkout_date + 30).to eq to subjectreturn_date
-  end
+  # it 'set a return date on every check out, 1 month from checkout date' do
+  #   expect(subject.checkout_date + 30).to eq to subjectreturn_date
+  # end
 
 end
