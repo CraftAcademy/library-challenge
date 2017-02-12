05b104783f8f3957343409797f521919cfbd9e42
felixottander <felix.ottander@yahoo.com>
2017-02-12 14:15:32 +0100

    changed all the tests except first one. Created some non finished methods to build some structure in the class Library.

--- lib/library.rb
+++ lib/library.rb
@@ -3,10 +3,49 @@ require 'date'
 
 class Library
 
-attr_accessor :item
+attr_accessor :items
 
 def initialize
-  @list = YAML.load_file('./lib/data.yml')
+  @items = YAML.load_file('./lib/data.yml')
 end
 
+#def check_out(item)    <=====Something i want to use later to check availability and raise error.
+#  case
+#  when
+#    if_not_available?(item) then
+#      puts 'Book is not available'
+#  else
+#  perform_checkout(item)
+#  end
+#end
+
+private
+
+def if_not_available?(item)
+  check_status(item, available)
+end
+
+def check_status(item, available)
+  @items.detect do |x|
+    if x[:item][:title] == item[:item][:title]
+      x[:available] = false
+end
+end
+
+def perform_checkout(item)
+  change_status(item, available: false, return_date: Date.today + 30) #<===Partial solution from class video
+#  @items.detect do |x|         <=====Why can't i do this instead?
+#  if x[:available] = false
+#  x[:return_date] =  Date.today + 40
+end
+end
+def change_status(item, args={})   #<=====Partial solution from class video
+  @items.detect do |x|
+    if x[:item][:title] == item[:item][:title]
+      x[:available] = args[:available]
+      x[:return_date] = args[:return_date]
+    end
+  end
+end
+end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,19 +2,34 @@ require './lib/library.rb'
 
 describe Library do
 
-  let(:person) { double('Person', checkout_books: true)}
+  let(:person) { double('Person', check_out: true, check_in: true)}
 
   it 'has a list of books on initialize' do
     library = Library.new
     expected_output = { title: '', author: '', available: '', return_date: '' }
   end
 
-  xit 'should be able to lend books to the person' do
+  it 'should be able to lend books to the person' do
+    subject.perform_checkout('Alfons och soldatpappan')
+    expected_output = { title: '', author: '', available: false, return_date: '' }
+    expect(subject('Alfons och soldatpappan')).to eq expected_output
+  end
 
+  xit 'should reject check_out if book is not available' do
+    subject.check_out('Skratta lagom! Sa pappa Åberg')
+    expect(subject('Skratta lagom! Sa pappa Åberg')).to eq 'Book is not available'
+  end
+
+  xit 'should be able to receive books from person' do
+    subject.check_in('Pettsson och Findus')
+    expected_output = { title: '', author: '', available: true, return_date: nil }
+    expect(subject('Pettsson och Findus')).to eq expected_output
   end
 
   xit 'should set a return date when lending books to person to be 30 days from now' do
-
+    subject.check_out('Pippi Långstrump')
+    expected_output = { title: '', author: '', available: '', return_date: Date.today + 30 }
+    expect(subject('Pippi Långstrump')).to eq expected_output
   end
 
 end
