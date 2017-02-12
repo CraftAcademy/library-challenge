d259559f2e68c977ce1662c8bfabc2b3c46b6e93
jocontreras <jen.almarasy@gmail.com>
2017-02-12 19:36:17 +0100

    check if the book is available or not

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,7 +2,17 @@ require 'yaml'
 require './lib/library.rb'
 
 describe Library do
-  it 'should have the list of available books' do
+  it 'should have the list of books' do
     expect(subject.bookrack).not_to be nil
   end
+
+  it 'should check if the book is available' do
+     expect(subject.bookrack[0][:available]).to be true
+  end
+
+  it 'should check if the book is not available' do
+     expect(subject.bookrack[2][:available]).to be false
+  end
+
+
 end
