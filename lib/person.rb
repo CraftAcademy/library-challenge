409325b5b3fbbc3e3b9ecd7d949a3ac9434d601b
felixottander <felix.ottander@yahoo.com>
2017-03-02 10:38:33 +0100

    Added test for person to return books in person spec, created successful methods to execute this in person.rb.

--- lib/person.rb
+++ lib/person.rb
@@ -9,4 +9,19 @@ attr_accessor :books
 def initialize
   @books = YAML.load_file('./lib/person.yml')
 end
+
+def perform_returnbook(item)
+  book_return(item, available: true)
+  return 'Book has been returned'
+end
+
+private
+
+def book_return(item, args)
+  @books.detect do |item|
+    if item[:item][:title] == item[:item][:title]
+      item[:available] == args[:available]
+    end
+  end
+end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,6 +3,10 @@ require './lib/person.rb'
 describe Person do
 
   it 'has a list of books on initialize' do
-    expected_output = { title: '', author: '', available: '', return_date: '' }
+    expected_output = { item: '', author: '', available: '', return_date: '' }
+  end
+
+  it 'should be able to return books to the library' do
+  expect(subject.perform_returnbook('Pettsson och Findus')).to eq 'Book has been returned'
   end
 end
