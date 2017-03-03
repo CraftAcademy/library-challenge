bc04b3099f8b41ca39a59c7212341777262e813a
johan <johan.schantz@telia.com>
2017-03-03 14:12:10 +0100

    added check out test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require './lib/library.rb'
 
-collection = YAML.load_file('./lib/data.yml')
+# collection = YAML.load_file('./lib/data.yml')
 describe Library do
 
   subject { described_class.new }
@@ -12,5 +12,9 @@ describe Library do
 
   it 'list of books currently available in the library' do
     expect(subject.available_books).not_to eq nil
+  end
+
+  it 'I should be able to allow individuals to check out a book' do
+    expect(subject.books).not_to eq nil
   end
 end
