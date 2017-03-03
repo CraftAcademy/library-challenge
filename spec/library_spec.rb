b652aa8eba21eb89e1a13d26c347571f8775cc61
casperjureskog <lcasper@mac.com>
2017-03-03 10:12:28 +0100

    add rspec test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require './lib/library.rb'
 
 collection = YAML.load_file('./lib/data.yml')
+person = YAML.load_file('./lib/person.yml')
 describe Library do
 
   subject { described_class.new }
@@ -31,11 +32,40 @@ describe Library do
     expect(collection[1][:return_date]).to eq date.to_s
   end
 
+  it 'set a return date on every check out, 1 month from checkout date' do
+    expect(collection[1][:book_id]).to eq 2
+  end
+
+
+  it 'allow individuals to check out a book' do
+    person[1][:available] = false
+    date = Date.today + 30
+    person[1][:return_date] = date.to_s
+    File.open('./lib/person.yml', 'w') { |f| f.write person.to_yaml }
+    expect(person[1][:available]).to eq false
+  end
+
+  it 'set a return date on every check out, 1 month from checkout date' do
+    expect(person[1][:book_id]).to eq 2
+  end
+
+  it 'set a return date on every check out, 1 month from checkout date' do
+    date = Date.today + 30
+    expect(person[1][:return_date]).to eq date.to_s
+  end
+
   it 'allow individuals to return books' do
     collection[1][:available] = true
     collection[1][:return_date] = 'in_house'
     File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
     expect(collection[3][:available]).to eq true
+  end
+
+  it 'allow individuals to return books' do
+    person[1][:available] = true
+    person[1][:return_date] = 'in_house'
+    File.open('./lib/person.yml', 'w') { |f| f.write person.to_yaml }
+    expect(person[3][:available]).to eq true
   end
 
 end
