f1c8e3898dc323da800ac2646e4450f411f0f847
johan <johan.schantz@telia.com>
2017-03-03 11:46:56 +0100

    list of all books in library added

---
+++ lib/library.rb
@@ -0,0 +1,15 @@
+require 'yaml'
+
+class Library
+
+  attr_accessor :books
+
+  def initialize
+    # @books = YAML.load_file('/lib/data.yml')
+    @person = YAML.load_file('./lib/person.yml')
+  end
+
+  def all_books
+    @books.to_a.each {|val| puts val }
+  end
+end
---
+++ spec/library_spec.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+require './lib/library.rb'
+
+collection = YAML.load_file('./lib/data.yml')
+describe Library do
+
+  subject { described_class.new }
+
+  it 'list all books in library' do
+    expect(subject.all_books).not_to eq nil
+  end
+end
