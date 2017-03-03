9a59360d7d9f3b50765699be57a5b058b3512d75
jocontreras <jen.almarasy@gmail.com>
2017-03-03 09:54:57 +0100

    added book checkout

--- lib/library.rb
+++ lib/library.rb
@@ -7,3 +7,26 @@ class Library
     @bookrack = YAML.load_file('./lib/booksdb.yml')
   end
 end
+
+def available_books
+  @bookrack.select{|obj| obj[:available] == true}.each {|val| puts val}
+end
+
+def test
+  @bookrack = @bookrack[2][:item]
+end
+
+def list_of_books
+  @bookrack.each {|val| puts val}
+end
+
+def patron_checkout(book_id)
+  book_id -=1
+  date = Date.today + 30
+  @bookrack[book_id][:available] = false
+  @bookrack[book_id][:return_date] = date.to_s
+  File.open('./lib/booksdb.yml','w') {|f| f.write @bookrack.to_yaml}
+  @patron[book_id][:available] = false
+  @patron[book_id][:return_date] = date.to_s
+  File.open('.lib/patron.yml','w'){|f| f.write @patron.to_yaml}
+end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,7 +1,10 @@
 require 'yaml'
 require './lib/library.rb'
+collection = YAML.load_file('./lib/booksdb.yml')
 
 describe Library do
+  #let(:patron) {double('Patron')}
+
   it 'should have the list of books' do
     expect(subject.bookrack).not_to be nil
   end
@@ -10,9 +13,21 @@ describe Library do
      expect(subject.bookrack[0][:available]).to be true
   end
 
-  it 'should check if the book is not available' do
-     expect(subject.bookrack[2][:available]).to be false
+  it 'should have a collection of books in the file' do
+    expect(collection).not_to eq nil
   end
+
+  it 'allows patron to check out a book' do
+    collection[1][:available] = false
+    date = Date.today + 30
+    collection[1][:return_date] = date.to_s
+    File.open('./lib/booksdb.yml', 'w') {|f| f.write collection.to_yaml}
+    expect(collection[1][:available]).to eq false
+  end
+
+
+
+
 
 
 end
