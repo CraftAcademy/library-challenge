3aae09267d8b3b111b683a1a3cc28a69144e2dfd
johan <johan.schantz@telia.com>
2017-03-03 13:44:22 +0100

    added list of avaiblable books

--- lib/library.rb
+++ lib/library.rb
@@ -5,11 +5,15 @@ class Library
   attr_accessor :books
 
   def initialize
-    # @books = YAML.load_file('/lib/data.yml')
+    @books = YAML.load_file('./lib/data.yml')
     @person = YAML.load_file('./lib/person.yml')
   end
 
   def all_books
     @books.to_a.each {|val| puts val }
+  end
+
+  def available_books
+    @books.select { |obj| obj[:available] == true  }
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,4 +9,8 @@ describe Library do
   it 'list all books in library' do
     expect(subject.all_books).not_to eq nil
   end
+
+  it 'list of books currently available in the library' do
+    expect(subject.available_books).not_to eq nil
+  end
 end
