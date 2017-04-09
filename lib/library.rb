2c9c3ba0e6a0bd3dced187e6ea1a4ca30ad33852
junydania <o.dania@aol.com>
2017-04-09 08:53:03 +0200

    Data.yml file created

---
+++ lib/library.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+class Library
+
+  attr_accessor :books
+
+  def initialize
+
+    @books = YAML.load_file('./lib/data.yml')
+
+  end
+
+
+
+
+
+
+end
---
+++ spec/library_spec.rb
@@ -0,0 +1,9 @@
+require 'date'
+require 'pry-byebug'
+require './lib/person.rb'
+require './lib/library.rb'
+
+
+describe Library do
+  it 'should check that '
+end
