79113c16bb0e343d958637c12e53419c60fb098e
Carissa <carissa@edgren.nu>
2017-06-17 23:52:22 +0200

    add title and author to test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,4 +5,10 @@ subject { described_class.new()}
   it 'should load available books from a file' do
   expect(subject.available_books.length).to eq 5
   end
+
+  it ' should display title and author of available books' do
+    first_book = subject.available_books[0]
+    expect(first_book["title"]).to be_truthy
+    expect(first_book["author"]).to be_truthy
+  end
 end
