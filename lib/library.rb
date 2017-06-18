1c86f5fb400cbc100e1f916ac549776859365449
Carissa <carissa@edgren.nu>
2017-06-18 15:14:30 +0200

    first story is finally green

--- lib/library.rb
+++ lib/library.rb
@@ -1,18 +1,24 @@
 require 'yaml'
 
 class Library
-  attr_accessor :available_books, :taken_books
+  attr_accessor :books
 
   def initialize
-    @available_books = load_item_info
-    @taken_books = []
+    @books = load_item_info
   end
   def load_item_info
      YAML.load_file('./lib/books.yml')
   end
   def print_item_info(item)
   item [:title] + " " + "by" + " " + item [:author]
-
   end
-
+  def available_books
+    temp_books = []
+    @books.each do |book_info|
+      if book_info[:available] == true
+        temp_books << book_info
+      end
+    end
+    temp_books
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,12 +2,12 @@ require './lib/library.rb'
 
 describe Library do
 subject { described_class.new()}
-  it 'should load available books from a file' do
-  expect(subject.available_books.length).to eq 5
+  it 'should load books from a file' do
+  expect(subject.books.length).to eq 5
   end
 
   it ' should display title and author of available books' do
-    first_item = subject.available_books[0]
+    first_item = subject.books[0]
     expect(first_item[:item][:title]).to be_truthy
     expect(first_item[:item][:author]).to be_truthy
   end
@@ -15,6 +15,9 @@ subject { described_class.new()}
     item = {"title":"Easy and Short", "author": "Katy Perry"}
     expected_printout = "Easy and Short by Katy Perry"
     expect(subject.print_item_info(item)).to eq expected_printout
-
+  end
+  it ' should list only available books' do
+  available_books = subject.available_books
+  expect(available_books.length).to eq 4
   end
 end
