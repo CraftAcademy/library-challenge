3b01fbd8561775bcc7edcea58946146f6f85ad11
faraznaeem <faraznaeem@gmail.com>
2017-06-18 15:08:04 +0200

    search for test

--- lib/library.rb
+++ lib/library.rb
@@ -1,4 +1,20 @@
+require 'yaml'
+
 class Library
 attr_accessor :title, :author,  :available, :return_date, :item
+
+def initialize (title, author, available, return_date, item)
+  @title = title
+  @author = author
+  @available = available
+  @return_date = return_date
+  @item = item
+
+
+def search_title(title)
+  puts "Title #{title}"
+end
+
+end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,4 +2,8 @@ require './lib/library.rb'
 
 describe do
 
+it "find a book" do
+  expect(title).to eq != nil
+end
+
 end
