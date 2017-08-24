11752369b206543667acbe23109668352a8c06fa
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-24 20:49:51 +0200

    modified the yaml test

--- lib/library.rb
+++ lib/library.rb
@@ -5,10 +5,14 @@ class Library
   attr_reader :book_list
 
   def initialize
-    @book_list = YAML.load_file('./lib/collection.yml')
+    @book_list = load_yaml('./lib/collection.yml')
   end
 
   def return_date(date)
     date.next_month
+  end
+
+  def load_yaml(file)
+    YAML.load_file(file)
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,16 +1,17 @@
 require './lib/library.rb'
 require 'date'
+require 'yaml'
 
 describe Library do
 
-    it 'YAML file should be loaded in book_list' do
-      substitude = lambda {subject.book_list[0][:item][:author]}
-      expect(substitude.call).to eq 'Terry Pratchett'
+    it 'properly setup YAML file should load correctly' do
+      expected_output = [{:item=>{:title=>"test book", author: "magnus"}, available: true, return_date: '2017-08-20'}]
+      expect(subject.load_yaml('./lib/testYaml.yml')).to eq expected_output
     end
 
-    it 'method to create a return date' do
+    it 'checks the return date' do
       date = Date.today
       due = Date.today.next_month
       expect(subject.return_date(date)).to eq due
     end
-end
+  end
