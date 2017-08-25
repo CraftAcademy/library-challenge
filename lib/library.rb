3b17e7995fb1c421798e9bca8180af1f51cb7292
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-25 21:35:32 +0200

    added return book method

--- lib/library.rb
+++ lib/library.rb
@@ -5,15 +5,19 @@ class Library
   attr_reader :book_list
 
   def initialize
-    @book_list = load_yaml('./lib/collection.yml')
+    @book_list = load_yaml
   end
 
   def return_date(date)
     date.next_month
   end
 
-  def load_yaml(file)
+  def load_yaml(file = './lib/collection.yml')
     YAML.load_file(file)
+  end
+
+  def write_to_yaml(list, file = './lib/collection.yml')
+    File.open(file)
   end
 
   def book_available(list)
@@ -32,7 +36,7 @@ class Library
       author = book[:item][:author]
       available = book[:available]
       return_date = book[:return_date]
-      puts "#{title} #{author} #{ available ? "available" : return_date}"
+      "#{title} #{author} #{ available ? "available" : return_date}"
     end
   end
 
@@ -44,6 +48,20 @@ class Library
       "The book is available and you need to return it no later than #{return_book}"
     else
       "That book is not available until #{borrow_book[:return_date]}"
+    end
+  end
+
+  def return_a_book(list, book)
+    return_book = list.detect { |obj| obj[:item][:title].include? book}
+    if return_book[:available] == true
+      "That book was not borrowed from here"
+    else
+      return_book[:available] = true
+      if return_book[:return_date] >= Date.today.to_s
+        "Thank you for returning the book"
+      else
+        "There is a 100kr fine for returning the book to late"
+      end
     end
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -7,14 +7,28 @@ describe Library do
     let(:list) { subject.load_yaml('./lib/testYaml.yml') }
 
     it 'properly setup YAML file should load correctly' do
-      expected_output = [{:item=>{:title=>'test book in',
+      expected_output = [{:item=>{:title=> 'test book in',
         author: 'magnus'},
         available: true,
         return_date: nil},
-        {:item=>{:title=>"test book not in",
-          :author=>"magnus"},
-          :available=>false,
-          :return_date=>"2017-09-20"}]
+        {:item=>{:title=> 'test book not in',
+          :author=> 'magnus'},
+          :available=> false,
+          :return_date=> '2017-09-20'}]
+      expect(subject.load_yaml('./lib/testYaml.yml')).to eq expected_output
+    end
+
+    xit 'should write to YAML file' do
+      list = subject.return_a_book(list, 'test book not in')
+      subject.write_to_yaml(list)
+      expected_output = [{:item=> {:title=> 'test book in',
+        :author=> 'magnus'},
+        :available=> true,
+        :return_date=> nil},
+        {:item=>{:title=> 'test book not in',
+          :author=> 'magnus'},
+          :available=> true,
+          :return_date=> nil}]
       expect(subject.load_yaml('./lib/testYaml.yml')).to eq expected_output
     end
 
@@ -25,46 +39,46 @@ describe Library do
     end
 
     it 'should print out a list of books' do
-      list = subject.load_yaml('./lib/testYaml.yml')
-      # expected_output = "#{'test book'} #{'magnus'} available"
-      expected_output = [{:item=>{:title=>"test book in",
-        :author=>"magnus"},
-        :available=>true,
+      expected_output = [{:item=> {:title=> 'test book in',
+        :author=> 'magnus'},
+        :available=> true,
         :return_date=> nil},
-        {:item=>{:title=>"test book not in",
-          :author=>"magnus"},
-          :available=>false,
-          :return_date=>"2017-09-20"}]
+        {:item=> {:title=> 'test book not in',
+          :author=> 'magnus'},
+          :available=> false,
+          :return_date=> '2017-09-20'}]
       expect(subject.list_books(list)).to eq expected_output
     end
 
     it 'should only print available books' do
-      list = subject.load_yaml('./lib/testYaml.yml')
-      expected_output = [{:item=>{:title=>"test book in",
-        :author=>"magnus"},
-        :available=>true,
+      expected_output = [{:item=> {:title=> 'test book in',
+        :author=> 'magnus'},
+        :available=> true,
         :return_date=> nil}]
-        expect(subject.book_available(list)).to eq expected_output
+      expect(subject.book_available(list)).to eq expected_output
     end
 
     it 'when are the books due to return' do
-      expected_output = [{:item=>{:title=>"test book not in",
-        :author=>"magnus"},
-        :available=>false,
+      expected_output = [{:item=> {:title=> 'test book not in',
+        :author=> 'magnus'},
+        :available=> false,
         :return_date=> '2017-09-20'}]
-        expect(subject.books_out(list)).to eq expected_output
+      expect(subject.books_out(list)).to eq expected_output
     end
 
     it 'can borrow a book and the return date is correct' do
-      list = subject.load_yaml('./lib/testYaml.yml')
       due = Date.today.next_month
       expected_output = "The book is available and you need to return it no later than #{due}"
       expect(subject.borrow_a_book(list, 'test book in')).to eq expected_output
     end
 
     it 'can not borrow book if not available' do
-      list = subject.load_yaml('./lib/testYaml.yml')
-      expected_output = "That book is not available until 2017-09-20"
+      expected_output = 'That book is not available until 2017-09-20'
       expect(subject.borrow_a_book(list, 'test book not in')).to eq expected_output
+    end
+
+    it 'return a book' do
+      list = YAML.load_file('./lib/testYaml.yml')
+      expect(subject.return_a_book(list, 'test book not in')).to eq "Thank you for returning the book"
     end
   end
