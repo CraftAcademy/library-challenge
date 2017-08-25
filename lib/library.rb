5e22c84491798e6b69ccdc04835699c7f79ab853
Andrea <andreademoja@gmail.com>
2017-08-25 12:46:14 +0200

    Added the possility to check if a book is available

--- lib/library.rb
+++ lib/library.rb
@@ -6,4 +6,16 @@ class Library
   def initialize
     @books = YAML.load_file('./lib/data.yml')
   end
+
+  def available?(title, author)
+    book_id = get_book(title, author)
+      return false unless book_id
+      book_id[:available]
+  end
+
+  private
+
+  def get_book(title, author)
+    books.detect { |obj| obj[:item][:title] == title && obj[:item][:author] == author }
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,7 +2,12 @@ require './lib/library.rb'
 require './lib/patron.rb'
 
 describe Library do
+
   it "Has an availale list of books" do
     expect(:books).not_to be_empty
-end
+  end
+
+  it "Checks that a book is available" do
+      expect(subject.available?("Alfons och soldatpappan", "Gunilla Bergström")).to be true
+  end
 end
