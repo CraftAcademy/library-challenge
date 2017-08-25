91de2fea7e34d3a8126d072baca8a157f3329850
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-25 10:01:54 +0200

    refactoring in methods but have broken code, still pushing.

--- lib/library.rb
+++ lib/library.rb
@@ -6,43 +6,59 @@ class Library
   collection = YAML.load_file('./lib/book_data.yml')
   exit = false
 
+
   while exit == false
 
     puts "Welcome to the library of your dreams. What would you like to do?
-    1. to list all books in library records
-    2. to list which books are available/unavailable
-    3. to searching for an author
-    4. to exit menu"
+    1. to list which books are available/unavailable
+    2. to searching for an author
+    3. to exit"
 
     n = gets.chomp.to_i
 
     case n
-    when 1
-      collection.each do |book|
-          puts "#{book[:item][:title]} by #{book[:item][:author]} (#{book[:item][:genre]})"
-        end
-    when 2
-      collection.each do |title|
-        if title[:available] == true
-          puts "available: #{title[:item][:title]} by #{title[:item][:author]} (#{title[:item][:genre]})"
-        else
-          puts "not available: #{title[:item][:title]} by #{title[:item][:author]} (#{title[:item][:genre]})"
-        end
+    when 1 then list_books
+    when 2 then search_author
+    when 3 then exit_program
+    else error_message
+    end
+
+    if exit == false
+      puts "press ENTER to return to menu"
+      x = gets.chomp
+      system "clear"
+    end
+  end
+
+  private
+
+  def list_books
+    collection.each do |title|
+      if title[:available] == true
+        puts "available: #{title[:item][:title]} by #{title[:item][:author]} (#{title[:item][:genre]})"
+      else
+        puts "not available: #{title[:item][:title]} by #{title[:item][:author]} (#{title[:item][:genre]})"
       end
-    when 3
-      puts "Which author do you want to search for?"
-      author = gets.chomp
+    end
+
+    def search_author
+      puts "Which author do you want to search for? Please enter first OR last name."
+      author = gets.chomp.capitalize
       result = collection.select { |obj| obj[:item][:author].include? author }
       result.each do |book|
         puts "#{book[:item][:title]} by #{book[:item][:author]} (#{book[:item][:genre]})"
       end
-    when 4
+    end
+
+    def exit_program
       exit = true
     end
-    if exit == false
-      puts "press any key to continue"
-      x = gets.chomp
-      system "clear"
+
+    def return_to_menu
+    end
+
+    def error_message
+      raise 'Choose correct number'
     end
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,12 +5,7 @@ describe Library do
     expect(subject.collection).to be_truthy
   end
 
-  it 'should list books' do
-    expected_output = "#{book[:item][:title]} by #{book[:item][:author]} (#{book[:item][:genre]})"
-    expect().to eq expected_output
-  end
-
-  it 'should list books that are available' do
+  it 'should list if books are available/unavailable' do
     list_available_books =
     expect()
   end
