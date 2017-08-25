2f71a848ea9e37a6f3674c3b0fee28554608cca4
basil <basil.mawejje@gmail.com>
2017-08-25 14:01:17 +0200

    Person can see list of books available in library

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,19 @@
-class Person
-  def initialize
+require 'date'
 
+class Person
+
+  attr_accessor :available_books, :shelf
+
+  def initialize
+    @shelf = YAML.load_file('./books/data.yml')
+    @available_books = @shelf.select { |obj| obj[:available]}
   end
 
-  private
-
-  def method_name
-
+  def available
+    if @available_books
+      'Book is available for borrowing'
+    else
+      'Book is not available for borrowing'
+    end
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,10 +2,10 @@ require './lib/person'
 
 describe Person do
   it 'can see a list of books currently available in the library' do
-
+    expect(subject.available).to eq 'Book is available for borrowing'
   end
 
   it 'can know when the book is supposed to be returned' do
-
+    expect(subject).not_to eq object
   end
 end
