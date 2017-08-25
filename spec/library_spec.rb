1676db5feb7efeaa0c60213aee71a7c0c101269f
Andrea <andreademoja@gmail.com>
2017-08-25 13:54:47 +0200

    return date of 1 month has been setted

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,5 +1,6 @@
 require './lib/library.rb'
 require './lib/patron.rb'
+require 'date'
 
 describe Library do
 
@@ -9,5 +10,10 @@ describe Library do
 
   it "Checks that a book is available" do
       expect(subject.available?("Alfons och soldatpappan", "Gunilla Bergström")).to be true
+  end
+
+  it 'is expected to have a return date' do
+  expected_date = Date.today.next_month(1).strftime("%m,%y")
+  expect(subject.return_date).to eq expected_date
   end
 end
