b14c8336bb05920d823bab72f843487a1746caf6
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-26 20:53:17 +0200

    added test to borrow_books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -30,6 +30,22 @@ describe Library do
     expect(subject.show_books_menu).to be_truthy
   end
 
+  it 'should be able to send borrow_book to available' do
+    index = 0
+    subject.collection[index][:available] == true
+    return_date = Date.today.next_month.strftime("%d/%m/%y")
+    subject.create_user('Amanda')
+    expect{subject.borrow_book(index)}.to output("You borrowed: The Winds of Winter by G.R.R. Martin. Return by: #{return_date}!\n").to_stdout
+  end
+
+  it 'should be able to send borrow_book to unavailable' do
+    index = 0
+    return_date = Date.today.next_month.strftime("%d/%m/%y")
+    subject.collection[index][:available] = false
+    subject.collection[index][:return_date] = Date.today.next_month.strftime("%d/%m/%y")
+    expect{subject.book_is_unavailable(index)}.to output("The Winds of Winter is unavailable. It will be returned by #{return_date}.\n").to_stdout
+  end
+
   it 'should borrow book if available' do
     index = 0
     return_date = Date.today.next_month.strftime("%d/%m/%y")
@@ -40,13 +56,9 @@ describe Library do
   it 'should tell you if book you want to borrow is unavailable' do
     index = 0
     return_date = Date.today.next_month.strftime("%d/%m/%y")
-    subject.collection[0][:available] = false
-    subject.collection[0][:return_date] = Date.today.next_month.strftime("%d/%m/%y")
+    subject.collection[index][:available] = false
+    subject.collection[index][:return_date] = Date.today.next_month.strftime("%d/%m/%y")
     expect{subject.book_is_unavailable(index)}.to output("The Winds of Winter is unavailable. It will be returned by #{return_date}.\n").to_stdout
-  end
-
-  it 'error message if searching for non existing author' do
-    expect{subject.error_message_no_match}.to output("No matching author.\n").to_stdout
   end
 
   it 'should be able to create_user' do
@@ -71,6 +83,15 @@ describe Library do
   it 'should exit menu' do
     expect{subject.exit_program}.to output("Come back soon, there\'s lots to read here!\n").to_stdout
   end
+
+  it 'should show error message no user when run' do
+    expect{subject.error_message_no_user}.to output("Create an user or log in first!\n").to_stdout
+  end
+
+  it 'should show error message if searching for non existing author' do
+    expect{subject.error_message_menu}.to output("Choose correct menu number, please.\n").to_stdout
+  end
+
 
   after do
     collection = YAML.load_file('./lib/book_data.yml')
