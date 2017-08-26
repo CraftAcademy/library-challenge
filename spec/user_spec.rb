574a3ce771333b48792b723f45f594e6cb0c8cb7
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-26 19:28:24 +0200

    test if array is empty on init. At least this works

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,11 +2,6 @@ require './lib/user.rb'
 
 describe User do
   it 'is expected to have no books on initialize' do
-    expect(subject.books).to eq nil
+    expect(subject.books).to match_array([])
   end
-
-  it 'should be able to see its books' do
-    expect(subject.borrowed_books).to eq subject.books
-  end
-
 end
