768ebbf374b59133845197a741af0942b95d2500
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-27 10:04:08 +0200

    test is expecting no name on init

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,4 +4,8 @@ describe User do
   it 'is expected to have no books on initialize' do
     expect(subject.books).to match_array([])
   end
+
+  it 'is expected to have no name on initialize' do
+    expect(subject.name).to be_nil
+  end
 end
