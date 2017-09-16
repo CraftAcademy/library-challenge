fb9daaeb2d451d2c5bf0cbf47fa313ad44bbc8fb
olofvi <olofvi89@gmail.com>
2017-09-16 13:13:07 +0200

    4 rspec going green

--- lib/library.rb
+++ lib/library.rb
@@ -12,6 +12,10 @@ class Library
     @return_date = Date.today.next_month
   end
 
+  def books
+    YAML.load_file('./lib/books.yml')
+  end
+
   def list_available
     YAML.load_file('./lib/books.yml').select { |obj| obj[:available] == true }
   end
@@ -20,15 +24,21 @@ class Library
     YAML.load_file('./lib/books.yml')
   end
 
+  def check_outdate
+    books.select{ |obj| obj[:item][:title]}
+    if books.select{ |obj| obj[:available] == false}
+      return @return_date
+    end
+  end
+
   def search_books
-    def search_books(title)
-      YAML.load_file('./lib/books.yml').select { |obj| obj[:item][:title] == title}
+    books.select { |obj| obj[:item][:title] == title}
   if books[:available] == false
-    'Not available'
+    raise 'Not available'
   else
     loan_book
   end
-end
+
 
 def loan_book
   books.select { |obj| obj[:available] == true}
@@ -37,22 +47,6 @@ def loan_book
   @books[:renter] = name
   change_books
 end
-
-
-def check_outdate
-  check = books.select{ |obj| obj[:item][:title].include?}
-  if check = [:available] == false
-    return @return_date
-  end
-end
-
-  def set_outdate
-    Date.today.next_month
-  end
-
-  def books
-    YAML.load_file('./lib/books.yml')
-  end
 
   def change_books
     File.open('./lib/books.yml', 'w') { |f| f.write books.to_yaml }
