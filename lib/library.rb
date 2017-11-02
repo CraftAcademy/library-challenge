64c6581074bd25acba07e8c537fb3d534f52e3b2
aljoh <reachingalfred@gmail.com>
2017-11-02 22:11:59 +0100

    added available books method

--- lib/library.rb
+++ lib/library.rb
@@ -4,4 +4,12 @@ class Library
   def initialize
     @collection = YAML.load_file('./lib/data.yml')
   end
+
+  def booklist
+    available_books = []
+    @collection.each do |item|
+      available_books << "#{item[:item][:title]} av #{item[:item][:author]}" if item[:available] == true
+    end
+    return available_books
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,10 +1,14 @@
 require './lib/library.rb'
 
 describe Library do
-   let(:person) { instance_double('Person') }
+   let(:person) { instance_double('Person', name: 'Alfred') }
 
    it 'has a collection of books on initialize' do
      expect(subject.collection).not_to be nil
+   end
+
+   it 'person can see a list of available books' do
+     expect(subject.booklist).to be_kind_of(Array)
    end
 
 end
