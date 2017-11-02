c2895a356832d6e2d364c47f04ce916f4656dd48
aljoh <reachingalfred@gmail.com>
2017-11-02 20:47:40 +0100

    added collection of books on initialize

---
+++ lib/library.rb
@@ -0,0 +1,7 @@
+require 'yaml'
+class Library
+  attr_accessor :collection
+  def initialize
+    @collection = YAML.load_file('./lib/data.yml')
+  end
+end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,10 @@
 require './lib/library.rb'
+
+describe Library do
+   let(:person) { instance_double('Person') }
+
+   it 'has a collection of books on initialize' do
+     expect(subject.collection).not_to be nil
+   end
+
+end
