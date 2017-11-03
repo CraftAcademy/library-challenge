1258e9d37659141efdffae1b9d102c20703a7baa
aljoh <reachingalfred@gmail.com>
2017-11-03 15:32:30 +0100

    added person name on initialize

---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+require_relative 'library'
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,2 +1,10 @@
 require './lib/person.rb'
 require './lib/library.rb'
+
+describe Person do
+  let(:library) { Library.new }
+  subject { described_class.new('Alfred')}
+  it 'has a name on initialize' do
+    expect(subject.name).to eq 'Alfred'
+  end
+end
