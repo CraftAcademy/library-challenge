9b3ff3280d4b0a04ca47c2133d1d878a010c9fa1
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-04 13:02:55 +0100

    Adding :books attribute to spec

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,17 @@
 class Person
 
-  attr_accessor :name
+  attr_accessor :name, :books
 
   def initialize(name)
     @name = name
   end
 
   def set_name
-    @name 
+    @name
   end
+
+#  def loan_books(books)
+#    @account == nil ? no_account : @account.balance += books
 
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,8 +8,21 @@ describe 'Person' do
     expect(subject).to respond_to(:name)
   end
 
-#  it 'should have a :books attribute with value of nil upon initialize' do
-#    expect(subject.books).to eq nil
+  it 'should have a :books attribute with value of nil upon initialize' do
+    expect(subject.books).to eq nil
   end
+
+=begin
+
+it 'can create an Account' do
+  before { subject.create_account }
+end
+
+
+  it 'can manage books record' do
+    let (:books) { Library.new }
+    before { subject.create_account }
+  end
+=end
 
 end
