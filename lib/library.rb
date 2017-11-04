598aedba6d5a10359a9dfc0d67252deee90a4575
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-04 15:29:29 +0100

    Expecting record of books

--- lib/library.rb
+++ lib/library.rb
@@ -5,6 +5,7 @@ class Library
   attr_accessor :books
 
   def initialize
-    @books = YAML.load_file(".lib/data.yml")
+    @books = YAML.load_file("./lib/data.yml")
   end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,13 +1,13 @@
-require './lib/libary'
+require './lib/library'
 require './lib/person'
 require 'yaml'
 
 describe Library do
 
-  let(:person) { instance_double('Person', name: 'Lisa' )}
-  subject { described class.new(name: 'Lisa') }
+#  let(:person) { instance_double('Person', name: 'Lisa', books: :item [0] ) }
+#  subject { described_class.new(name: 'Lisa' )}
 
   it 'is expected to have a record of books on initialize' do
-    expect(subject.books).not_to be_empty
+    expect(subject.books).not_to be nil
   end
 end
