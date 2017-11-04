acf94cee0dd80f3a8f8e805caa4bc95bd400a778
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-04 14:25:32 +0100

    Added set_name method

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,29 @@
+#require './lib/data.yml'
+#require "yaml"
+
 class Person
 
   attr_accessor :name, :books
 
   def initialize(name)
-    @name = name
+    @name = set_name(name)
+#    @books = books
   end
 
-  def set_name
-    @name
+  def set_name(obj)
+    obj == nil ?  missing_name : @name = obj
   end
+
+  def missing_name
+    raise "A name is required"
+  end
+
+#  def list_of_books(obj)
+#    @books
+#  end
 
 #  def loan_books(books)
 #    @account == nil ? no_account : @account.balance += books
 
-  end
+#  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,23 +1,29 @@
 require './lib/person'
 
-describe 'Person' do
+describe Person do
 
   subject { described_class.new(name: 'Lisa') }
 
-  it 'should have a name' do
-    expect(subject).to respond_to(:name)
+#  it 'should have a name' do
+#    expect(subject).to respond_to(:name)
+#  end
+
+  it 'is expected to have a :name on initialize' do
+    expect(subject.name).not_to be nil
   end
 
-  it 'should have a :books attribute with a collection of books upon initialize' do
-    expect(subject.books).not_to be_empty 
+  xit 'is expected to raise error if no name is set' do
+    expect { described_class.new }.to raise_error 'A name is required'
+  end
+
+  xit 'should have a :books attribute with a collection of books upon initialize' do
+    expect(subject.books).not_to be_empty
   end
 
 =begin
-
-it 'can create an Account' do
-  before { subject.create_account }
-end
-
+  it 'can create an Account' do
+    before { subject.create_account }
+  end
 
   it 'can manage books record' do
     let (:books) { Library.new }
