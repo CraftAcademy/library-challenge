91edde53978f5c88e15745b96ae6f7e6505d8d17
Francesco <francesco.falchi@gmail.com>
2017-11-05 23:07:51 +0100

    Remove unnecessary code

--- lib/library.rb
+++ lib/library.rb
@@ -22,41 +22,5 @@ class Library
   result.each do |book|
     puts "#{book[:item][:title]} author: #{book[:item][:author]}"
   end
-  def check_out
-    case
-    when [:available] == false then {message: 'Not available'}
-    else
-      check_out_active(item)
-    end
   end
-
-  end
-
-  private
-
-  def set_today
-    Date.today.strftime('%F')
-  end
-
-   def return_date
-     Date.today.next_month.strftime('%F')
-   end
-
-   def collection_status(item, args={})
-     @collection.detect do |i|
-       if i[:item][:title] == item[:item][:title]
-         i[:available] = args[:available]
-         i[:return_date] = args[:return_date]
-       end
-     end
-   end
-
-   def check_out_active(item)
-     collection_status(item, available: false, return_date: return_date)
-     update_yaml_file
-   end
-
-   def update_yaml_file
-     File.open('./lib/data.yml', 'w') { |f| f.write self.items.to_yaml }
-   end
 end
