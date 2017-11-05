e95671a23f8605a8ac5d167ed6ee7808a96a453b
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-05 14:03:33 +0100

    Added pseudo class info

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,17 +3,14 @@ require 'yaml'
 
 describe Person do
 
-  subject { described_class.new(name: 'Lisa' ) }
+  subject { described_class.new(name: 'Lisa', books: [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>"05/12/17"}] ) }
 
   it 'is expected to have a :name on initialize' do
     expect(subject.name).not_to be nil
   end
 
-=begin
-  it 'can manage books record' do
-    let (:books) { Library.new }
-    before { subject.create_account }
+  it 'is expected that person can see their checked out books and due dates' do
+    expect(subject.add_book("Alfons och soldatpappan")).to be_kind_of(Array)
   end
-=end
 
 end
