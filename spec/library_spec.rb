a47b9967233617dcb249df08240276debdadf100
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-05 10:39:22 +0100

    Add test for library

---
+++ spec/library_spec.rb
@@ -0,0 +1,10 @@
+
+
+describe Library do
+  let(:person) { instance_double('Person') }
+
+  it 'has a collection of books on initialize' do
+    expect(subject.collection).not_to be nil
+  end
+
+end
