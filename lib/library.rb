ef621189f0200f78fb4f28a721c45ba932f7c076
bollen <david.boll@me.com>
2018-01-26 15:18:18 +0100

    added yaml-connection

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,14 @@
+require 'yaml'
+
 class Library
-  
+
+  attr_accessor :books
+
+  def initialize
+    #@books = YAML::load_file(File.join(__dir__, 'data.yml'))
+  end
+
 end
+
+#books = YAML::load_file(File.join(__dir__, 'data.yml'))
+#puts @books[0][title]
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,10 +1,20 @@
 require './lib/library.rb'
 
+=begin
+“As a Library,
+I want to be have books,
+so that people can check out books”
+=end
+
 describe Library do
   #initial testing to get us started
   it 'can create an instace of a library' do
     library = Library.new
     expect(library).to be_kind_of Library
+  end
+
+  it 'responds to "books"' do
+    expect(subject).to respond_to(:books)
   end
 
 end
