ff0c11f9737bc42b63029536bc280a789b3f9660
limei <limei@evolve.se>
2018-04-07 12:47:38 +0200

    fixed library

--- lib/library.rb
+++ lib/library.rb
@@ -1,4 +1,10 @@
 
 class Library
 
+  attr_accessor :book_list
+
+  def initialize
+    @book_list = [{name: "little women", author: "Jane Austen"}]
+
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,13 +1,13 @@
 require './lib/library.rb'
 
-describe 'library' do
+describe Library do
 
   it 'is an array' do
-    expect(book_list).to be_instance_of(Array)
+    expect(subject.book_list).to be_instance_of(Array)
   end
 
   it 'return a collection of 1 or more books' do
-    expect(book_list.length).to be > 0
+    expect(subject.book_list.length).to be > 0
   end
 
 end
