0b1cf0820cdfad1178d9914b40a9b6cc566e35b4
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-04-07 19:36:30 +0200

    refactoring and added info to data.yml and started checkout function

--- lib/library.rb
+++ lib/library.rb
@@ -1,17 +1,18 @@
 require 'yaml'
 
 class Library
+  attr_accessor :books
 
   def initialize
-    books = YAML.load_file('./lib/data.yml')
+    @books = YAML.load_file('./lib/data.yml')
   end
 
-  def list_all
-    puts books
+  def books_available
+    @books.select { |x| x[:available] == true }
   end
 
-  def list_available
-    puts books.select { |x| x[:available] == true }
+  def checkout(id, person)
+    @books.detect { |x| x[:id] == id } =
   end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -4,7 +4,7 @@ describe Library do
 
 it 'displays the list of books'do
   expected_output = YAML.load_file('./lib/data.yml')
-  expect(subject.list).to eq expected_output
+  expect(subject.books).to eq expected_output
 end
 
 end
