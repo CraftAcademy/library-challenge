b7a1dadb464fc34d98ee982afdc7d7854c52aed8
limei <limei@evolve.se>
2018-04-08 22:34:11 +0200

    current books

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ require './lib/library.rb'
 
 class Person
 
-  attr_accessor :name
+  attr_accessor :name, :current_books
 
 
   def initialize(attrs = {})
@@ -11,8 +11,7 @@ class Person
     end
 
     @name = attrs[:name]
-
-
+    @current_books = [ ]
   end
 
   def perform_check_out(attrs = {})
@@ -20,5 +19,16 @@ class Person
       raise 'Need a library'
     end
     result = attrs[:library].perform_check_out(attrs[:book_id])
+    @current_books.push(result)
+    result
   end
+
+  def perform_return(attrs = {})
+    if attrs[:book_id] == nil
+      raise 'Need a book_id'
+    end
+    attrs[:library].perform_return(attrs[:book_id])
+    @current_books.delete_if { |b| b[:book_id] == attrs[:book_id] }
+  end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,6 +14,21 @@ describe Person do
   it 'can check_out a book' do
     library = Library.new
     expected_output = { status: true, message: 'check_out complete', book_id: 1234, date: Date.today, return_date: Date.today.next_month }
-     expect(subject.perform_check_out({book_id:1234, library: library})).to eq expected_output
+    expect(subject.perform_check_out({book_id:1234, library: library})).to eq expected_output
   end
+
+  it 'adds the book I check out to my list of current books' do
+     library = Library.new
+     subject.perform_check_out({book_id:1234, library: library})
+     expect(subject.current_books.length).to be > 0
+  end
+
+  it 'removes the book I return from my list of current books' do
+    library = Library.new
+    subject.perform_check_out({book_id:1234, library: library})
+    subject.perform_return({book_id:1234, library: library})
+    expect(subject.current_books.length).to eq 0
+  end
+
+
 end
