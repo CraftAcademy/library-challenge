49d33dac4de77f74ed1cc74e24c46fcfe010d452
Erik <erik.steinman@hotmail.com>
2018-06-15 12:08:51 +0200

    WIP modify person.rb and person_spec.rb

--- lib/person.rb
+++ lib/person.rb
@@ -3,15 +3,21 @@ require 'library.rb'
 
 class Person
     
-    attr_accessor :books
+    attr_accessor :books, :loaned, :collection
 
     def initialize
         @books = list_of_books
+        @loaned = loaned_books
+        @collection
     end
 
     def list_of_books
         collection = YAML.load_file('./lib/data.yml')
         collection = true
+    end
+
+    def loaned_books 
+        collection[0][:available] = false
     end
 
 
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,12 +2,18 @@
 require './lib/person.rb'
 
 describe Person do
+    
+let(:library) { instance_double('Person', collection: )}
+
 
     it 'is expected to be books on initialize' do
         expect(subject.books).to eq true
     end
 
-    
+    it 'is expected that a Person has books' do
+    expect(subject.loaned).to be true
+    end
+
 
 
 
