3615fbe40d12ce29fbefb79db4b75b0cbff31004
rupertlion <rupertlion@gmail.com>
2018-08-30 16:44:22 +0200

    Initialising and organising lib and spec files

---
+++ spec/indidivual_spec.rb
@@ -0,0 +1,10 @@
+require './lib/individual.rb'
+
+# As an individual
+# In order to get my hands on a good book
+# I would like to see a list of books currently available in the library
+# with information about the title and author
+
+# As an individual
+# In order to avoid awkward moments at the library
+# I would like to know when my book is supposed to be returned
---
+++ spec/library_spec.rb
@@ -0,0 +1,14 @@
+require './lib/library.rb'
+
+# As a library
+# In order to have good books to offer to the public
+# I would like to be able to have a collection of books
+
+# As a library
+# In order to have good books to offer to the public
+# I would like to be able to allow individuals to check out a book
+
+# As a library
+# In order to make the books available to many individuals
+# I would like to set a return date on every check out
+# and I would like that date to be 1 month from checkout date
