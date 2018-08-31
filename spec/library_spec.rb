81d2c22a4f1b55b9329c5749f846397469ef8699
rupertlion <rupertlion@gmail.com>
2018-08-31 15:08:34 +0200

    Added user stories to spec files

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -7,3 +7,15 @@ describe Library do
     end
 
 end
+
+# As a librarian
+# In order to know what books we are able to lend to visitors
+# I would like to be able to be able to see the availability of books in the collection
+
+# As a librarian
+# In order to update the availability of books in the collection
+# I would like to be able to allow individuals to check out a book
+
+# As a librarian
+# In order to make the books available to other visitors in a timely manner
+# I would like to set a return date 1 month from checkout date
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -8,4 +8,11 @@ describe Visitor do
 
 end
 
+# As a visitor
+# In order to choose a book to borrow
+# I would like to see a list of books currently available in the library
+# with information about the title and author
 
+# As an visitor
+# In order to avoid unwanted fines, and to generally do the right thing
+# I would like to know when my book is supposed to be returned
