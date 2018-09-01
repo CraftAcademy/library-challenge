defe9d8c416d777a7659f5d5e6dc61c0b3e389df
danmalmx <danmalmx@gmail.com>
2018-09-01 22:19:18 +0200

    adding commented out tests / impl with errors

--- lib/library_challenge.rb
+++ lib/library_challenge.rb
@@ -5,10 +5,19 @@ class Library
     attr_accessor :book_list
 
     def initialize
-        @book_list = 'data.yml'
+        @book_list = YAML.load_file('./lib/data.yml')
     end
 
-    # def book_list
-    #     collection = YAML.load_file('./lib/data.yml')
-    # end
+    def book_search(search_term)
+        book_return = book_list.detect { |bok| bok[:item][:title].include? search_term } #|| book_list.detect { |bok| bok[:item][:author].include? search_term }
+        # print "Here is what we found:"
+        # puts "Title: #{search_term[:item][:title]}"
+        # puts "Author: #{search_term[:item][:author]}"
+        # if search_term[:available] = false 
+        # puts "Not available, back on #{search_term[:return_date]}"
+        # else 
+        #     puts "Available"
+        # end
+    end
+
 end
--- spec/library_challenge_spec.rb
+++ spec/library_challenge_spec.rb
@@ -2,12 +2,14 @@ require './lib/library_challenge.rb'
 
 describe Library do
 
-   # let[:] { instance_double("") }
+    #let[:title] { instance_double("title") }
 
     it "has a list of books" do
-    expect(subject.book_list).to eq 'data.yml'
+    expect(subject.book_list).to eq YAML.load_file('./lib/data.yml')
     end
 
-    it "book lists available books" do
-        expect(subjet.)
+    # it "books are searchable by title" do
+    # expect(subject.book_search).to eq book_search
+    # end
+
 end
