7fdb352ccec031c098cf02983e45de607f95b589
rupertlion <rupertlion@gmail.com>
2018-09-01 14:37:43 +0200

    Library initialized with passing collection

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,11 +1,28 @@
 require './lib/library.rb'
+require 'yaml'
 
 describe Library do
 
-    it '' do
-        
+    let(:visitor) { instance_double('Visitor', name: 'Rupert') }
+ 
+    it 'has a collection of books in the library on initialize' do
+        expected_output = YAML.load_file('./lib/data.yml')
+        expect(subject.collection).to eq expected_output
     end
 
+    # it 'can see a list of all AVAILABLE books in the library collection' do
+    #     all_books = YAML.load_file('./lib/data.yml')
+    #     expected_output = all_books.select { |obj| obj[:available] == true  }
+    #     expect(subject.check_availability).to eq expected_output
+    # end
+
+    # it 'can see the TITLE and AUTHOR of all AVAILABLE books' do
+    #     all_books = YAML.load_file('./lib/data.yml')
+    #     available_book_list = all_books.select { |obj| obj[:available] == true  }
+    #     expected_output = available_book_list.map {|book| book.values[0]}
+    #     subject.check_availability
+    #     expect(subject.check_available_titles).to eq expected_output
+    # end
 end
 
 # As a librarian
