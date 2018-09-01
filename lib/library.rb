6c2b8c0b29119596c03bd64957f5ac6c16fd790b
Angelica Costantini <angelicacostantini07@gmail.com>
2018-09-01 12:04:25 +0200

    first test library fail

--- lib/library.rb
+++ lib/library.rb
@@ -1,17 +1,22 @@
 require 'yaml'
-require './book'
 require 'date'
-class Library
-end
 
-
-require 'yaml'
-require 'date'
 
 class Library
 
+attr_accessor :collection
 
+    def initialise
+        @collection = YAML.load_file('./lib/data.yml')
+        # @return_date = Date.today + 1.month 
 end
+
+def book_collection(collection)
+    @collection
+end 
+end
+
+
 
 
 #Need method for return date
@@ -19,7 +24,7 @@ end
 # 1. Search for the item in library
 # 2. Check out the ite
 
-                person.rb
+                # person.rb
                 # checked out books go to book_shelf variable
 # Search for book example
 #  collection.detect { |obj| obj[:item][:title] == "Pippi Långstrump"  
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -10,8 +10,21 @@
 # 2In order to make  the  books  available  to many  individuals
 # 3I would  like to set areturndate on every  check  out
 # 4and I would  like  that  date to be 1 month  from  checkout  date
+require './lib/library.rb'
 
 describe library do
-    let(:person) {instance_double('Person', name: 'Thomas')}
-    subject { described_class.new(name: 'Thomas')}
+    let(:person) {instance_double('Person', name: 'Bob')}
+    
+    # before do
+    #     allow(person).to receive(:collection)
+    # end
+
+    it 'person can view books in collection on initialise' do
+        expect(:collection).to be true 
+    end
 end
+
+
+# it 'check book available in collection' do
+#     book.available = true 
+# end
