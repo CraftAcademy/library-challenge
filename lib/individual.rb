3ed4e56aedf8ab24e1126efff0cfaa07c47a7b44
camron <camronmoradi@gmail.com>
2018-11-01 12:03:59 +0100

    Create individual class

---
+++ lib/individual.rb
@@ -0,0 +1,2 @@
+class Individual
+end
---
+++ spec/individual_spec.rb
@@ -0,0 +1,11 @@
+require './lib/individual.rb'
+require './lib/library.rb'
+
+describe Individual do
+
+    it 'see a list of books currently available in the library' do
+        expected_output = {title: 'Harry Potter'}
+        expect(Library.new.books).to eq expected_output
+    end
+
+end
