a6f90991cc31dcf9034c5ada7335d9463761ee60
Aditya Naik <naik.aditya@gmail.com>
2018-11-02 22:52:29 +0100

    Added and passed a test to list books available in the library

--- lib/library.rb
+++ lib/library.rb
@@ -7,4 +7,12 @@ class Library
         @collection = YAML.load(File.read('./lib/data.yml'))
     end
 
+    def display_available_books(collection)
+        available_collection = []
+        collection.each do |item|
+            item[:available] ? available_collection << item : false
+        end
+        return available_collection
+    end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -10,4 +10,11 @@ describe Library do
         expect(subject.collection).to be_a_kind_of(Array)
     end
 
+    it 'shows a list of available books to customer' do
+    books_array = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Skratta lagom! Sa pappa Åberg", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>"2016-05-25"}] 
+    expected_output = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}]
+    expect(subject.display_available_books(books_array)).to eq expected_output
+    end
+
+
 end
