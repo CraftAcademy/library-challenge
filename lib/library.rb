3ac01751475de0a42bf8592ef06e311c61b73876
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-02 12:29:07 +0100

    adding a method to add a book and store it in a variable called book

--- lib/library.rb
+++ lib/library.rb
@@ -1,9 +1,19 @@
 class Library
     require 'yaml'
-    attr_accessor :collection
+    require 'date'
+    attr_accessor :collection, :book
+    RETURN_DATE = 30
 
     def initialize
         @collection = {}
+        @book = {}
+    end
+
+    def add_book(arg = {})
+        @book[:title] = arg[:title]
+        @book[:author] = arg[:author]
+        @book[:availability] = 'available'
+        @book[:return_date] = nil
     end
 end
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,7 +1,11 @@
 require './lib/library.rb'
+require 'pry'
 
 describe Library do
 
-    
+    it 'expect to add a book with title, author, availability, and empty return date ' do
+        subject.add_book({title:'Harry Potter', author: 'JK Rowling'})
+        expect(subject.book).to eq ({title:'Harry Potter', author: 'JK Rowling', availability: 'available', return_date: nil})
+    end
 
 end
