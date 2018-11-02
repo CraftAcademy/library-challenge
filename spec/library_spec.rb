9db050785a3374226166caad893f98d8cf5dd246
Aditya Naik <naik.aditya@gmail.com>
2018-11-02 22:58:05 +0100

    resolving merge conflict in library_spec.rb file. All tests are now passing

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,10 +9,6 @@ describe Library do
     it 'has a list of books' do
         expect(subject.collection).to be_a_kind_of(Array)
     end 
-    it 'shows list of availible books' do
-        expected_outcome = collection.detect { |obj| obj[:item][:availible] == true}
-        expect(subject.collection).to match expected_outcome
-    end
 
     it 'shows a list of available books to customer' do
     books_array = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Skratta lagom! Sa pappa Åberg", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>"2016-05-25"}] 
