0385e933dadcca7fb0f94fdbaf92182009ca036b
William Schneiderman <william.m.schneiderman@gmail.com>
2018-11-03 14:27:02 +0100

    added all users stories to tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -4,6 +4,10 @@ describe Library do
 
     let(:customer) { instance_double('Person', user_id: '123456', pin_code: '1234',reading_list: [] )}
 
+    # As a library
+    # In order to have good books to offer to the public
+    # I would like to be able to have a collection of books
+
     it 'has books' do
         expect(subject.collection).not_to be_empty
     end
@@ -33,7 +37,12 @@ describe Library do
         expect(subject.select_book(books_array,'1')).to eq expected_output
     end 
 
-    it 'allows an individual to check out a book' do
+    # As a library
+    # In order to make the books available to many individuals
+    # I would like to set a return date on every check out
+    # and I would like that date to be 1 month from checkout date
+
+    it 'allows an individual to check out a book for one month' do
         selected_book = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}
         expected_output = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=> '2018-12-03'}
         expect(subject.book_checkout(selected_book,'123456','1234', customer)).to eq expected_output
@@ -45,5 +54,7 @@ describe Library do
         subject.book_checkout(selected_book,'123456','1234', customer)
         expect(customer.reading_list).to eq reading_list
     end
+
+   
 
 end
