c1858de065b84be8e33b7c4027967e5ee0adb3cc
Aditya Naik <naik.aditya@gmail.com>
2018-11-03 10:41:38 +0100

    Every person instance has a randomly generated user id and password

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
+
+
 class  Person
+    attr_accessor :user_id, :pin_code
+    def initialize(params = {})
+
+        set_user_parameters()
+               
+    end
+
+    private
+
+    def set_user_parameters()
+        @user_id = rand(100000..999999)
+        @pin_code = rand(1000..9999)
+    end
      
 end
+
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,4 +2,9 @@ require './lib/person.rb'
 
 describe Person do
 
+    it 'every customer has a user_id and a pin_code' do
+        expect(subject.user_id).not_to be nil
+        expect(subject.pin_code).not_to be nil
+    end
+
 end
