2b1e8da55840c0402b0559d0e342d30d52ab57bf
Aditya Naik <naik.aditya@gmail.com>
2018-11-04 12:50:32 +0100

    Added test and code for saving updated library collection data to file, cleaned up tests and code

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,20 @@
 require 'yaml'
+require './lib/person.rb'
 
 class Library
     attr_accessor :collection
 
     def initialize 
-        @collection = YAML.load(File.read('./lib/data.yml'))
+        @collection = load_file()
+    end
+
+    def load_file
+        YAML.load_file('./lib/data.yml')
+    end
+
+    def save_to_file(collection)
+        File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
+        return true
     end
 
     def display_available_books(collection)
@@ -12,23 +22,30 @@ class Library
         collection.each do |item|
             item[:available] ? available_collection << item : false
         end
-        return available_collection
+        available_collection
     end
 
-    def select_book(available_collection,selection_position)
-        selected_book = []
-        selected_book << available_collection[0]
+    def select_book(available_collection,selection_position) 
+         available_collection[selection_position - 1]
     end 
 
-    def book_checkout(selected_book,username,pin_code,customer) 
+    def book_checkout(selected_book,username,pin_code,customer,collection) 
         selected_book[:available] = false
         selected_book[:return_date] = (Date.today + 30).to_s 
         add_to_customer_reading_list(selected_book,username,pin_code,customer)
+        update_library_collection(selected_book,collection)
         selected_book
     end
 
     def add_to_customer_reading_list(selected_book,username,pin_code,customer)
         customer.reading_list << selected_book
+    end
+
+    def update_library_collection(selected_book,collection)
+        collection.each do |book|
+            book[:item][:title] == selected_book[:item][:title] ? book.each{ |key,value| book[key] = selected_book[key]} : false
+        end
+        save_to_file(collection)
     end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,25 +1,22 @@
 require './lib/library.rb'
+require './lib/person.rb'
+require 'date'
 
 describe Library do
 
-    let(:customer) { instance_double('Person', user_id: '123456', pin_code: '1234',reading_list: [] )}
+    # let(:customer) { instance_double('Person', user_id: '123456', pin_code: '1234',reading_list: [] )}
+    customer = Person.new
+    Date_of_book_return = (Date.today + 30).to_s
 
     # As a library
     # In order to have good books to offer to the public
     # I would like to be able to have a collection of books
 
-    it 'has books' do
-        expect(subject.collection).not_to be_empty
-    end
-
     it 'has a list of books' do
+        expect(subject.collection).not_to be_empty
         expect(subject.collection).to be_a_kind_of(Array)
+        expect(subject.load_file()).to eq subject.collection
     end 
-
-    # it 'can save a list of books' do
-        # collection =[]
-       # expect(subject.save_to_file(collection)).to eq true
-    # end
 
     # As a library
     # In order to have good books to offer to the public
@@ -33,8 +30,8 @@ describe Library do
 
     it 'allows an individual to select a book from availble book list' do
         books_array = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}, {:item=>{:title=>"Skratta lagom! Sa pappa Åberg", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}] 
-        expected_output = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}]
-        expect(subject.select_book(books_array,'1')).to eq expected_output
+        expected_output = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}
+        expect(subject.select_book(books_array,1)).to eq expected_output
     end 
 
     # As a library
@@ -44,15 +41,27 @@ describe Library do
 
     it 'allows an individual to check out a book for one month' do
         selected_book = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}
-        expected_output = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=> '2018-12-03'}
-        expect(subject.book_checkout(selected_book,'123456','1234', customer)).to eq expected_output
+        expected_output = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=> Date_of_book_return}
+        expect(subject.book_checkout(selected_book,'123456','1234', customer,subject.collection)).to eq expected_output
     end
 
     it 'when a person checks out a book, it is added to the reading list' do
+        customer.reading_list = []
         selected_book = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}
-        reading_list =[{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=> '2018-12-03'}]
-        subject.book_checkout(selected_book,'123456','1234', customer)
+        reading_list =[{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=> Date_of_book_return}]
+        subject.book_checkout(selected_book,'123456','1234', customer,subject.collection)
         expect(customer.reading_list).to eq reading_list
+    end
+
+    it 'when a person checks out a book, it is updated in the yaml file' do
+        
+        customer.reading_list = []
+        selected_book = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}
+        reading_list =[{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=> Date_of_book_return}]
+        checked_out_book = reading_list[0]
+        subject.book_checkout(selected_book,'123456','1234', customer,subject.collection)
+        expect(subject.save_to_file(subject.collection)).to eq true
+        expect(subject.collection).to include checked_out_book
     end
 
    
