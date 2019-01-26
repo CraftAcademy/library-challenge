a9780b31b456763bfbc445f1c51c890da295e813
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-26 14:12:57 +0100

    adds initial requires and class

---
+++ lib/library.rb
@@ -0,0 +1,15 @@
+require 'yaml'
+
+class Library
+    attr_accessor :title, :author, :available, :return_date, :item
+
+    def initialize
+        @title = 
+        @author = 
+        @available = 
+        @return_date = 
+        @item = 
+    end
+
+
+end
