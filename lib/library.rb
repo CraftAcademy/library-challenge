7e763e6e83a892589c1ef662446f17b1ac5a8eed
Per Eriksson <per_hegg@hotmail.com>
2019-01-26 17:36:53 +0100

    adds checkout_book

--- lib/library.rb
+++ lib/library.rb
@@ -3,12 +3,16 @@ require 'date'
 
 class Library
  
-    attr_accessor :collection
+    attr_accessor :collection, :checked_out, :available
 
     def initialize
         @collection = YAML.load_file('./lib/data.yml')
+        @checked_out = show_unavailable_books
+        @available = show_available_books
+        
 
     end
+    
 
     def show_available_books
 
@@ -20,23 +24,45 @@ class Library
         available_books
     end
 
+    def show_unavailable_books
+
+        unavailable_books = []
+        @collection.each do |item| 
+        
+        item[:available] == false ? unavailable_books << item : false
+        end
+        unavailable_books
+    end
+
+
+    
+
     def select_book(title)
 
         @collection.select { |obj| obj[:item][:title].include? title  }
         
-        
     end
+
     
-    def checkout_book
-
-    end
-
-    def write_to_file
-
+    
+    def checkout_book(index)
+        if
+        @collection[index][:available] == false
+        print "Already checked out"
+        else
+        puts "Whats your name?"
+        name = gets.strip
+        @collection[index][:checked_out_by]= name
+        @collection[index][:available]= false
+        @collection[index][:return_date]= Date.today.next_month
         File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
+        print "Thank you! Please return within a month"
+        end
         
-    
+
     end
+
+    
 
 end
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -4,4 +4,8 @@ describe Library do
     it 'have books in library' do
         expect(subject.collection).not_to eq nil
     end
+
+    it 'can search a book in the database' do
+        expect(subject.select_book("PC")).not_to eq nil)
+    end
 end
