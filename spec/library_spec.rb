01e3d8c86a6f00433b2cd1da119a64d1293c1695
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-27 21:46:23 +0100

    adds testing for title search

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -14,8 +14,12 @@ describe Library do
         expect(subject.show_available_books).not_to eq nil
     end
 
-    it 'can show which books are unavailable' do
+    it 'can show which books are checked out' do
         expect(subject.show_unavailable_books).not_to eq nil
+    end
+
+    it 'can search for a specific book title' do
+        expect(subject.select_book("Clean Code")).not_to eq nil
     end
     
 end
