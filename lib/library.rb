4e3cd81fbd3af668a4bf1b72f7a3df8d892acb50
Alecia <aleciabenjamin@gmail.com>
2019-01-27 20:29:23 +0100

    clean up and refactor

--- lib/library.rb
+++ lib/library.rb
@@ -2,28 +2,29 @@ require 'date'
 
 class Library
     STANDARD_VALIDITY_DAYS = 30
-    attr_accessor :collection, :person
+    attr_accessor :collection
 
     def initialize 
         @collection = YAML.load_file('./lib/data.yml') 
-        @person = person
     end
 
-    def display_title_author
+    def get_book_index(book_title)
+        title_author = @collection.map {|obj| obj[:item]}
+        titles = title_author.map {|obj| obj[:title]}
+        index =titles.index(book_title)
+    end
+
+    def title_author
         @collection.map {|obj| obj[:item]}
     end
 
     def is_available(book_title)
-        title_author = @collection.map {|obj| obj[:item]}
-        titles = title_author.map {|obj| obj[:title]}
-        index =titles.index(book_title)
+        index = get_book_index(book_title)
         @collection[index][:available] ? true : false
     end
 
-    def has_return_date(book_title)
-        title_author = @collection.map {|obj| obj[:item]}
-        titles = title_author.map {|obj| obj[:title]}
-        index =titles.index(book_title)
+    def set_return_date(book_title)
+        index = get_book_index(book_title)
         @collection[index][:return_date] ? return_date : false
     end
 
@@ -32,9 +33,7 @@ class Library
     end
 
     def check_out(book_title)
-        title_author = @collection.map {|obj| obj[:item]}
-        titles = title_author.map {|obj| obj[:title]}
-        index =titles.index(book_title)
+        index = get_book_index(book_title)
         if @collection[index][:available] then
             @collection[index][:available] = false
             date_new = @collection[index][:return_date] = return_date
@@ -48,9 +47,7 @@ class Library
     end
 
     def check_in(book_title)
-        title_author = @collection.map {|obj| obj[:item]}
-        titles = title_author.map {|obj| obj[:title]}
-        index =titles.index(book_title)
+        index = get_book_index(book_title)
         if @collection[index][:available] == false then
             @collection[index][:available] = true
             File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,7 +9,7 @@ describe Library do
     end
     
     it "displays books' title and author" do
-        expect(subject.display_title_author).to eq ([{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, {:title=>"Skratta lagom! Sa pappa Åberg", :author=>"Gunilla Bergström"}, {:title=>"Osynligt med Alfons", :author=>"Gunilla Bergström"}, {:title=>"Pippi Långstrump", :author=>"Astrid Lindgren"}, {:title=>"Pippi Långstrump går ombord", :author=>"Astrid Lindgren"}])
+        expect(subject.title_author).to eq ([{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, {:title=>"Skratta lagom! Sa pappa Åberg", :author=>"Gunilla Bergström"}, {:title=>"Osynligt med Alfons", :author=>"Gunilla Bergström"}, {:title=>"Pippi Långstrump", :author=>"Astrid Lindgren"}, {:title=>"Pippi Långstrump går ombord", :author=>"Astrid Lindgren"}])
     end
 
     it 'changes availability to false when book is checked out' do
@@ -25,7 +25,7 @@ describe Library do
     it 'displays return date as 30 days from check out date' do
         subject.check_out("Alfons och soldatpappan")
         return_date = Date.today.next_day(30).strftime('%Y/%m/%d')
-        expect(subject.has_return_date("Alfons och soldatpappan")).to eq return_date
+        expect(subject.set_return_date("Alfons och soldatpappan")).to eq return_date
     end
 
     it 'issues a receipt with return date when book is checked out' do
@@ -36,48 +36,7 @@ describe Library do
         expect(subject.check_in("Osynligt med Alfons")).to be_a_kind_of(Hash)
     end
 
-
-    #it 'is expected to raise error if book is unavailable' do
-        #subject.check_out("Alfons och soldatpappan")
-        #expect(subject.unavailable_book).to raise_error 'The selected book is not available'   
-    #end
-    #it 'displays return date as 30 days from check out date' do
-           #allow(book).to receive(:return_date).and_return('27/02/2019')
-            #expected_output = { : false, message: 'card expired', date: Date.today }
-            #expect(subject.withdraw(6, '1234', account)).to eq expected_output
-        #end
-
-    #it 'sets up a return date when checked out' do
-        #expected_output = { available: false, message: 'success', date: Date.today }
-        #expect(subject.set_return_date()).to eq expected_output
-    #end
 end
 
-#it can be false when the book is not available / true if available
 
-    #it 'gives a receipt when book is checked out' do
-        #expected_output = { status: false, message: 'success', date: Date.today }
-        #expect(subject.check_out()).to eq expected_output
-    
-
-    #it 'gives a receipt when book is checked in' do
-        #expected_output = { status: true, message: 'success', date: Date.today }
-        #expect(subject.check_in(45, '1234', account)).to eq expected_output
-    #end
-#end
-
-   # 1)
-   # item[:available] => true / false
-   # check in -> change_status method true // checkout -> change_status false
-   # receipt: when successfully checked in or out
-
-   # 2)
-   # date_today 
-   # - when check out - set 30 days return date
-
-   # 3) check_availability -> list of titles and authors (all books with availability or .filter and show only t4he available ones)
-   # 4) when check in set return_date to '' (empty string)
-
-
-    
    
