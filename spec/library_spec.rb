536738574b394666fb70f7454de9712ef3f0c98b
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-27 21:37:21 +0100

    adds spec for testing which books are available

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,17 +1,17 @@
 require './lib/library.rb'
 
 describe Library do
-    let(:person) {instance_double('Person, name: 'Amanda', rented_book: 'Clean Code')}
 
     it 'have books in library' do
         expect(subject.collection).not_to eq nil
     end
 
     it 'can search a book in the database' do
-        expect(subject.select_book("PC")).not_to eq nil)
+        expect(subject.select_book("PC")).not_to eq nil
     end
 
     it 'can show which books are available' do
-        expect(subject.show_available_books).
+        expect(subject.show_available_books).not_to eq nil
+    end
     
 end
