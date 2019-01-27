09fe6548fc2df5cd23f2b2e5e5ee4f04b182dddb
your_name <patricia.ebaka@gmail.com>
2019-01-27 19:41:27 +0100

    add status unavailable error

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -18,4 +18,9 @@ describe Library do
         expected_output = YAML.load_file('./lib/data.yml').select { |book| book[:item][:title].include? 'Pippi' }
         expect(subject.search_title('Pippi')).to eq expected_output
     end
+
+    it 'allows person to borrow a book and change status to unavailable ' do
+        expected_output = YAML.load_file('./lib/data.yml')
+        File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
+        expect(subject.available).to include [:available]
 end
