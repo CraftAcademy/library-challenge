ac5c94c405c0bade154ffc1aaeabbf335493978e
felix_bonnier <felix_bonnier@yahoo.com>
2019-04-06 13:06:21 +0200

    adds method for checking books that have return dates, and one test

--- lib/library.rb
+++ lib/library.rb
@@ -1,14 +1,21 @@
 require 'yaml'
 
 class Library
-    attr_accessor :collection, :available_books
+    attr_accessor :collection
     def initialize
         @collection = YAML.load_file('./lib/data.yml')
-        @available_books = show_available_books
     end
-end
 
 def show_available_books
     available_books = @collection.select { |obj| obj[:available] == true }
-    available_books
+end
+
+def show_books_with_return_dates
+    available_books = @collection.select { |obj| obj[:return_date] }
+end
+
+private
+def due_date
+end
+    
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,18 +1,24 @@
 require './lib/library.rb'
 
 describe Library do
+    subject {described_class.new}
 
     it 'should have a collection of books when initialized' do
     expect(subject.collection).to be_truthy
     end
 
     it 'should be possible to display what books are available' do
-    expect(subject.available_books).to be_truthy
+    expect(subject.show_available_books).to be_truthy
     end
 
     it 'should display correct amount of available books' do
-    expect(subject.available_books.length).to eq 4
+    expect(subject.show_available_books.length).to eq 4
     end
+    
+    it 'should be able to display all books with a return date' do
+    expect(subject.show_books_with_return_dates.length).to eq 1
+    end
+
 
 end
 
