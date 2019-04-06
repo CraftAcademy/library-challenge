d0b09f6015e4e78fea9bfff86dd130828f236cfc
zanenkn <zane.neikena@gmail.com>
2019-04-06 12:49:17 +0200

    Adds unit test for access_books method and implements the code in the Library class

--- lib/library.rb
+++ lib/library.rb
@@ -2,5 +2,11 @@ require "date"
 require "yaml"
 
 class Library
+    def initialize
+    end
+
+    def access_books
+        YAML.load_file("./lib/books.yml")
+    end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,6 +2,12 @@ require "date"
 require "yaml"
 require "./lib/library.rb"
 
+
 describe Library do
+    it "can read the YAML file with all the books" do
+    collection = YAML.load_file("./lib/books.yml")
+    expect(subject.access_books).to eq collection 
+    end
+
 
 end
