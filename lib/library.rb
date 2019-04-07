36ba893f07a44d2fd012ba593b35eb32262c9aa1
felix_bonnier <felix_bonnier@yahoo.com>
2019-04-07 14:18:18 +0200

    adds checkout book method, with updated availability saved to database. All tests green

--- lib/library.rb
+++ lib/library.rb
@@ -2,28 +2,32 @@ require 'yaml'
 require 'date'
 
 class Library
-    attr_accessor :collection 
+    attr_accessor :collection, :available_books, :non_available_books
     def initialize
         @collection = YAML.load_file('./lib/data.yml')
         @available_books = show_available_books 
         @non_available_books = show_non_available_books
-    
     end
 
+def save_updates
+    File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
+end
+
 def show_available_books
-    @collection.select { |obj| obj[:available] == true }
+    collection.select { |obj| obj[:available] == true }
 end
 
 def show_non_available_books
-    @collection.select { |obj| obj[:available] == false  }
+    collection.select { |obj| obj[:available] == false  }
 end
 
-def select_book(title)
-    selected_book = @collection.select { |obj| obj[:item][:title] == title }
+def select_book_by_title(title)
+    collection.select { |obj| obj[:item][:title] == title }
 end
 
+def checkout_book(book)
+    collection[book][:available] = false
+    save_updates
+    return "You have borrowed #{collection[book][:item][:title]}"
 end
-
-library = Library.new
-puts library.select_book('Skratta lagom! Sa pappa Åberg')
-
+end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,7 +3,7 @@ require './lib/library.rb'
 
 describe Library do
     subject {described_class.new}
-    let(:person) { instance_double('Person')}
+    let(:person) { instance_double('Person', name: 'Boa')}
 
     it 'should have a collection of books when initialized' do
     expect(subject.collection).to be_truthy
@@ -17,17 +17,27 @@ describe Library do
     expect(subject.show_available_books.length).to eq 4
     end
     
-    it 'should be able to display all books with a return date - non available books' do
+    it 'should be able to display all books that are NOT available' do
     expect(subject.show_non_available_books.length).to eq 1
     end
 
     it 'should be able to select a book by title' do
     expected_output = [{:item=>{:title=>"Skratta lagom! Sa pappa Åberg", :author=>"Gunilla Bergström"},:available=>false, :return_date=>"2016-05-25"}]
-    expect(subject.select_book("Skratta lagom! Sa pappa Åberg")).to eq expected_output
+    expect(subject.select_book_by_title("Skratta lagom! Sa pappa Åberg")).to eq expected_output
     end
-    
 
+    it 'should be able to make it possible to checkout a book, and update the item availability' do
+    subject.checkout_book(0)
+    expect(subject.collection[0][:available]).to eq false
+    subject.collection[0][:available] = true
+    subject.save_updates
+    end
+
+    it 'should get a success message when book is checked out' do
+    expect(subject.checkout_book(0)).to eq "You have borrowed Alfons och soldatpappan"
+    subject.collection[0][:available] = true
+    subject.save_updates
+    end
 end
-
 
 
