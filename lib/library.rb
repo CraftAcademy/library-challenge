7b53c00a7d48baf26b45dbd351a8024f3dead6ca
erka <erik.westerberg@hotmail.se>
2019-06-15 14:01:31 +0200

    Written som more tests on the status of the book

--- lib/library.rb
+++ lib/library.rb
@@ -5,4 +5,12 @@ class Library
     def initialize
         @books = YAML.load_file('./lib/inventory.yml')
     end
+
+    def book_status(book)
+        @books = 
+
+    # it 'return avaiable if the status of book is avaiable' do
+    #     expect(subject.books_status).to eq :availeble
+    # end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,14 +1,26 @@
 require "./lib/library.rb"
 
 describe Library do
-    
+    let(:visitor) { class_double('visitor', name: 'erik') }
     it 'can read the YAML file with all the books' do
         expected_output = YAML.load_file('./lib/inventory.yml')
         expect(subject.books).to eq expected_output
     end
 
-    # it 'must return the status of the book' do
-    #     expected_output =
-    #     expect(subjevt)
-    # end
+    it 'return avaiable if the status of book is avaiable' do
+        expect(subject.books_status).to eq :availeble
+    end
+
+    it 'return borrowed if the status of the book is non-available' do
+        expect(subject.books_status).to eq :borrowed
+    end
+
+    it 'is expected to raise error if the book does not exist in the library' do
+        # expect { described_class.new }.to raise_error 'The book does not exist'
+    end
+    
+    it 'is expected to have an return date on initialize' do
+        expected_date = Date.today.next_month(1).strftime("%d/%m")
+        expect(subject.return_date).to eq expected_date
+    end
 end
