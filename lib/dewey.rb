73204c0feed86ed999351ffd73889c2534596024
Max Aubain <max.aubain@gmail.com>
2019-06-15 10:44:44 +0200

    shell class files

---
+++ lib/dewey.rb
@@ -0,0 +1,29 @@
+# import files and built-in methods using <require('name')>
+require 'date'
+require './lib/librarian'
+require './lib/member'
+
+#class definition
+class Dewey
+
+    #attributes accessor to easily change instance variables
+    attr_accessor 
+
+    #give instance of class initial attributes
+    def initialize()
+
+    end
+
+    #public methods to be accessed by user in irb or other classes
+    def xxx
+    
+    end
+
+    #private methods to be accessed only by methods in this class
+    private
+    def xxx
+
+    end
+
+
+end
