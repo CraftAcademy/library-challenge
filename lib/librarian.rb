9d392dbf8497757db15f51a730dd4497b48f689a
Daria Lykova <daria.lykova@gmail.com>
2019-06-15 15:03:18 +0200

    Writing tests for book search & checkout functions/Initializing methods for each/Successfully running first rspec tests

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -1,9 +1,20 @@
-class Library
+require 'pry'
+
+class Librarian
 
     attr_accessor :books
 
     def initialize
         @books = 10
     end
+
+    def search_books(title, author)
+        {title: 'title', author: 'author'} 
+    end
+    
+    def checkout_books(title, author)
+        {title: 'title', author: 'author', date: Date.today}
+    end
+        
     
 end
--- spec/librarian_spec.rb
+++ spec/librarian_spec.rb
@@ -1,12 +1,20 @@
-require './lib/library.rb'
+require './lib/librarian.rb'
+require 'pry'
 
-describe Library do
 
-    it 'loads a collection of books upon initialize' do
-        expect(subject.test_library).to eq 1234
-    end
+describe Librarian do
 
     it 'has to have 10 books' do
         expect(subject.books).to eq 10
+    end
+
+    it 'has a search function' do
+        book_info = {title: 'title', author: 'author'}
+        expect(subject.search_books('title', 'author')).to eq book_info
+    end
+
+    it 'has to be able to check out books' do
+        receipt = {title: 'title', author: 'author', date: Date.today}
+        expect(subject.checkout_books('title', 'author')).to eq receipt
     end
 end
