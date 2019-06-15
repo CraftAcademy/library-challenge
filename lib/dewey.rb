d0949e7eeaeaa353a4ddda5d93bce433748b5342
Max Aubain <max.aubain@gmail.com>
2019-06-15 18:51:36 +0200

    Dewey instance functions add_member_name, add_return_date, and not_available for book check out feature:

--- lib/dewey.rb
+++ lib/dewey.rb
@@ -4,32 +4,45 @@ require 'yaml'
 class Dewey
 
     #attributes accessor to easily change instance variables
-    attr_accessor :books, :available_books
+    attr_accessor :books, :available_books, :return_date, :available
 
     #give instance of class initial attributes
     def initialize
-        @books = YAML.load_file('./lib/collection.yml')
+        @books = YAML.load_file('./lib/collection_original.yml')
         available_books?
     end
     
-    def checkout_book(title)
-        add_return_date(title)
-        not_available(title)
-        available_books?
+    def checkout_book(book_num, member)
+        add_return_date(book_num)
+        add_member_name(book_num, member)
+        not_available(book_num)
+        available_books? #update available books list after checkout
+        raise 'Your book(s) are due back in one month!'
     end
 
-    private
+    #Private
+    #These functions can be private in practice, but for testing purposes they are not
 
     def available_books?
         @available_books = @books.select{ |obj| obj[:available] == true}
     end
 
-    def add_return_date(title)
-        #modify .yml file at selected title to add return date
+    def add_member_name(book_num, member)
+        book_num -= 1
+        @books[book_num][:member] = member.name
+        File.open('./lib/collection_new.yml', 'w') { |f| f.write books.to_yaml }
     end
 
-    def not_available(title)
-        #modify .yml file at selected title remove availability
+    def add_return_date(book_num)
+        book_num -= 1
+        @books[book_num][:return_date] = Date.today.next_month.strftime('%Y-%m-%d')
+        File.open('./lib/collection_new.yml', 'w') { |f| f.write books.to_yaml }
+    end
+
+    def not_available(book_num)
+        book_num -= 1
+        @books[book_num][:available] = false
+        File.open('./lib/collection_new.yml', 'w') { |f| f.write books.to_yaml }
     end
 
 
--- spec/dewey_spec.rb
+++ spec/dewey_spec.rb
@@ -2,7 +2,24 @@
 require 'date'
 require './lib/dewey.rb'
 
-describe Dewey do
 
+describe Dewey do
+    subject { described_class.new }
+    let (:member) {instance_double('mem', name: "Max")}
+
+    it 'adds member name to a checked out book' do 
+        subject.add_member_name(1, member)
+        expect(subject.books[0][:member]).to eq "Max"
+    end
+    
+    it 'adds a return date to a checked out book' do
+        subject.add_return_date(1)
+        expect(subject.books[0][:return_date]).to eq Date.today.next_month.strftime('%Y-%m-%d')
+    end
+
+    it 'marks a checked out book as unavailable' do 
+        subject.not_available(1)
+        expect(subject.books[0][:available]).to eq false
+    end
 
 end
