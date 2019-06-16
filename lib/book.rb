0e91bd3b984c3857cdf68138c9028b9304fb42c6
pedro_bras <pedro.c.bras@gmail.com>
2019-06-16 18:22:05 +0200

    working on add new book to list method

--- lib/book.rb
+++ lib/book.rb
@@ -5,18 +5,34 @@ class Book
     attr_accessor :available, :title, :author, :return_date
 
     def initialize(attrs = {})
-        @title = attrs[:title]
-        @author = attrs[:author]
+        set_title(attrs[:title])
+        set_author(attrs[:author])
         @available = true
         @return_date = nil #changing from due_date to return_date since that is the attribute in the data.yaml file. Also adding nil cause a book shouldn't have a return date when initialized
     end
 
-    def add_book
+    def set_title(obj)
+        obj == nil ? missing_title : @title = obj.to_s
+    end
+
+    def missing_title
+        raise "A title is required"
+    end
+
+    def set_author(obj)
+        obj == nil ? missing_author : @author = obj.to_s
+    end
+
+    def missing_author
+        raise "An author is required"
+    end
+
+    def add_book #is it possible to create an instance of the book (ex. newBook) and then add that book to the list with this function with book.add_book?
         new_book = { 
             item: [ { title: @title, author: @author } ],
             available: @available,
             return_date: @return_date }
-        File.open("./lib/data.yaml", "w") { |file| file.write(new_book.to_yaml) }
+        File.open("./lib/data.yml", "w") { |file| file.write(new_book.to_yaml) }
     end
 
 #How can we link this class to the data.yaml file so that the new books
