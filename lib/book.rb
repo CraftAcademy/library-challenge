ea0d7c06c0dd240a85118aaea26542b6eeffda4b
piavw <piavonwachenfelt@hotmail.com>
2019-09-06 14:39:15 +0200

    Changes name of the original library.rb and library_spec.rb tto book.rb and book_spec.rb, creates new library lib- and spec-file for the new Library class.

---
+++ lib/book.rb
@@ -0,0 +1,20 @@
+class Book  
+    STANDARD_VALIDITY_DAYS = 30
+    attr_accessor :author, :title, :return_date, :book_status
+
+    def initialize
+        @author = :author
+        @title = :title
+        @return_date = set_return_date
+        @book_status = :available
+    end
+
+    private
+    def set_return_date
+        Date.today.next_day(STANDARD_VALIDITY_DAYS).strftime('%d/%m/%y')
+    end
+
+    def self.unavailable(book)
+        book.book_status = :unavailable
+    end
+end
--- lib/library.rb
+++ lib/library.rb
@@ -1,22 +1,6 @@
-class Book  
-    STANDARD_VALIDITY_DAYS = 30
-    attr_accessor :author, :title, :return_date, :book_status
-
+class Library
+    attr_accessor :books
     def initialize
-        @author = :author
-        @title = :title
-        @return_date = set_return_date
-        @book_status = :available
+        @books = :books
     end
-
-    private
-    def set_return_date
-        Date.today.next_day(STANDARD_VALIDITY_DAYS).strftime('%d/%m/%y')
-    end
-
-    def self.unavailable(book)
-        book.book_status = :unavailable
-    end
-
-
 end
