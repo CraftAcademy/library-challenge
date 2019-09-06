910911ff8ea0f508f4b1a73174e22f4b978cd894
piavw <piavonwachenfelt@hotmail.com>
2019-09-06 12:51:58 +0200

    Creates title instance variable, creates return date formula and reference tests.

--- lib/library.rb
+++ lib/library.rb
@@ -1,8 +1,17 @@
+
+
 class Book  
-    attr_accessor :author
+    STANDARD_VALIDITY_DAYS = 30
+    attr_accessor :author, :title, :return_date
 
     def initialize
         @author = :author
+        @title = :title
+        @return_date = set_return_date
     end
 
+    private
+    def set_return_date
+        Date.today.next_day(STANDARD_VALIDITY_DAYS).strftime('%d/%m/%y')
+    end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,8 +1,19 @@
 require './lib/library.rb'
+require 'Date'
 
 describe Book do
     it 'has an author' do
         expect(subject.author).to eq(:author)
     end
+
+    it 'has a title' do
+        expect(subject.title).to eq (:title)
+    end
+
+    it 'has a 30 day return date from check-out' do
+        expected_date = Date.today.next_day(30).strftime('%d/%m/%y')
+        expect(subject.return_date).to eq expected_date
+    end
+
 
 end
