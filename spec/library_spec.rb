1e35c928f5038d4e93edf409d9556ea712f03911
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-06 17:46:07 +0200

    Add test for library_spec, print author and title

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,7 +6,11 @@ describe Library do
   end
 
   it 'needs to print title and author name' do
-    book = 
+    book = 'Gunilla Bergström : Skratta lagom! Sa pappa Åberg'
+
+    command = lambda { "#{subject.database[1][:item][:author]} : #{subject.database[1][:item][:title]}" }
+    expect(command.call).to eq book
+  end
 
 
   #needs to print out a list of books with titles and author names
