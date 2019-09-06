f2c16b84944e8fdc66d92434580a88a77a7cc6ee
Becca <beccaburns89@gmail.com>
2019-09-06 12:40:48 +0200

    Creates author instance variable and reference test.

---
+++ lib/library.rb
@@ -0,0 +1,8 @@
+class Book  
+    attr_accessor :author
+
+    def initialize
+        @author = :author
+    end
+
+end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,8 @@
 require './lib/library.rb'
+
+describe Book do
+    it 'has an author' do
+        expect(subject.author).to eq(:author)
+    end
+
+end
