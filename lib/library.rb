252d9f09d77f0b520330995fe2754d58a2297bba
Becca <beccaburns89@gmail.com>
2019-09-07 12:46:33 +0200

    Creates when statements, Adds short codes for books, Creates status change methods for individual books, WIP - continues to remove all data in yml file while testing to make availability false.

--- lib/library.rb
+++ lib/library.rb
@@ -1,6 +1,14 @@
 require 'yaml'
 
 class Library
+attr_accessor :t0, :t1, :t2, :t3, :t4
+    def initialize
+       t0 = "Alfons och soldatpappan"
+       t1 = "Skratta lagom! Sa pappa Åberg"
+       t2 = "Osynligt med Alfons"
+       t3 = "Pippi Långstrump"
+       t4 = "Pippi Långstrump går ombord"
+    end
 
     def collection
         collection = YAML.load_file('./lib/data.yml') 
@@ -12,38 +20,66 @@ class Library
 
     def search_by_title(book_name)
         collection.select { |book| book[:item][:title].include? "#{book_name}" }
+        { message: 'Please do a new search with the full book title within quotation marks.' }
     end
+
+
+    def book_change_availability_false(book_name)
+        case  
+            when change_available_status_false?(t0)
+                { message: 'You have checked-out the book. The return date is', date: Date.today.next_day(30).strftime('%d/%m/%y') }
+
+            when change_available_status_false?(t1)
+                { message: 'You have checked-out the book. The return date is', date: Date.today.next_day(30).strftime('%d/%m/%y') }
+
+            when change_available_status_false?(t2)
+                { message: 'You have checked-out the book. The return date is', date: Date.today.next_day(30).strftime('%d/%m/%y') }
+
+            when change_available_status_false?(t3)
+                { message: 'You have checked-out the book. The return date is', date: Date.today.next_day(30).strftime('%d/%m/%y') }
+
+            when change_available_status_false?(t4)
+                { message: 'You have checked-out the book. The return date is', date: Date.today.next_day(30).strftime('%d/%m/%y') }
+            else
+                print 'wrong'
+        end
+    end
+
     
     private
-    def change_available_status_false #this code works in irb and rspec (do not touch)
+
+    def change_available_status_false?(t0)
         collection.select { |book| book[:item][:title].include? "Alfons och soldatpappan" }
         collection[0][:available] = false
         File.open('./lib/data.yml', 'w') { |book| book.write collection.to_yaml}
         collection.select { |book| book[:item][:title].include? "Alfons och soldatpappan" }
     end
 
-    
-
-    #[WIP]
-=begin
-    def something #look at the withdraw in the atm!??
-    when unavaiable(book)
-        {subject.book == checked_out ? unavailable : available} #????
-    end
-
-    def unavailable(book)
-        @book == nil ? checked_out : available
-    end
-    
-    def checked_out
-        raise 'Book is checked out'
-        collection[0][:available] = false
+    def change_available_status_false?(t1)
+        collection.select { |book| book[:item][:title].include? "Skratta lagom! Sa pappa Åberg" }
+        collection[1][:available] = false
         File.open('./lib/data.yml', 'w') { |book| book.write collection.to_yaml}
+        collection.select { |book| book[:item][:title].include? "Skratta lagom! Sa pappa Åberg" }
     end
 
-    def available(book) # the 0 needs to change depending on which book we are changing/checking.
-        collection[0][:available] = true
+    def change_available_status_false?(t2)
+        collection.select { |book| book[:item][:title].include? "Osynligt med Alfons" }
+        collection[2][:available] = false
         File.open('./lib/data.yml', 'w') { |book| book.write collection.to_yaml}
+        collection.select { |book| book[:item][:title].include? "Osynligt med Alfons"  }
     end
-=end
+
+    def change_available_status_false?(t3)
+        collection.select { |book| book[:item][:title].include? "Pippi Långstrump" }
+        collection[3][:available] = false
+        File.open('./lib/data.yml', 'w') { |book| book.write collection.to_yaml}
+        collection.select { |book| book[:item][:title].include? "Pippi Långstrump" }
+    end
+
+    def change_available_status_false?(t4)
+        collection.select { |book| book[:item][:title].include? "Pippi Långstrump går ombord" }
+        collection[4][:available] = false
+        File.open('./lib/data.yml', 'w') { |book| book.write collection.to_yaml}
+        collection.select { |book| book[:item][:title].include? "Pippi Långstrump går ombord" }
+    end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,13 +1,10 @@
 require './lib/library.rb'
+require 'date'
 
 describe Library do
     
     it 'has the ability to show list of checked-out books' do
         expect(subject.collection).to include{:item}
-    end
-
-    it 'change_available_status to change availability to false' do
-        expect(subject.collection[0][:available]).to eq false
     end
 
     it 'can search for author by input of argument' do
@@ -18,5 +15,10 @@ describe Library do
     it 'can search for title by input of argument' do
         subject.search_by_title("pippi")
         expect(subject).to be_truthy
+    end
+
+    it 'sets chosen book availability to false' do
+        expected_output = { message: 'You have checked-out the book. The return date is', date: Date.today.next_day(30).strftime('%d/%m/%y') }
+        expect(subject.book_change_availability_false("Alfons och soldatpappan")).to eq expected_output
     end
 end
