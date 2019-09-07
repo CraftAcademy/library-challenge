abfce5426234fff653cf0291fa002ba11b7aeae1
luca <l.lobacher@web.de>
2019-09-07 13:17:53 +0200

    Adds method to list books in alphabetical order
    Adds test to list the books' :author, :title :availability

--- lib/library.rb
+++ lib/library.rb
@@ -7,6 +7,15 @@ class Library
     @database = load_database
   end
 
+  def list_all_books
+    books = []
+    (0...data.length).each do |book|
+      books.push "#{@database[book][:item][:author]}: #{@database[book][:item]}, Available: #{@database[book][:available]}"
+    end
+    books.sort!
+    boock.each {|book| puts book}
+  end
+
   private
   def load_database
     YAML.load_file('lib/data.yml')
