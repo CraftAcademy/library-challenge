1681fd1216b5b3f27c06d0e35e646947c6f50a3b
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-08 11:32:29 +0200

    Refactor methods in User class and add TODOs

--- lib/user.rb
+++ lib/user.rb
@@ -7,7 +7,8 @@ class User
     @data = load_data
   end
 
-  def find_available_books
+  def see_available_books
+    # TODO: Update same method in Library class
     available_books = []
 
     (0...data.length).each do |book|
@@ -15,25 +16,24 @@ class User
         available_books << data[book]
       end
     end
-    return available_books
-  end
-
-  def see_available_books
-    available_books = find_available_books
+    
     (0...available_books.length).each do |book|
       puts available_books[book][:item][:author] + ' : ' +  available_books[book][:item][:title]
     end
   end
 
-  def search_for_book_title(title)
-    available_books = find_available_books
-    (0...available_books.length).each do |book|
-      if title == available_books[book][:item][:title]
-        puts 'The book "' + title + '" is available'
-        return true
-      end
-    end
-    puts 'The book "' + title + '" is not available'
+  def search_for_book_title(search_str)
+    puts data.select { |book| book[:item][:title].include? search_str}
+    # TODO: Fix output and put into Library class
+  end
+
+  def search_for_book_author(search_str)
+    puts data.select { |book| book[:item][:author].include? search_str}
+    # TODO: Fix output and put into Library class
+  end
+
+  def checkout
+    # TODO: Finish process
   end
 
   private
