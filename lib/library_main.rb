7b4974b435ad55277a35197fd21db1c081776654
your_name <your@email.com>
2019-11-17 15:48:46 +0100

    adds checkout without date

--- lib/library_main.rb
+++ lib/library_main.rb
@@ -9,14 +9,22 @@ class Library_main
     end
 
     def check_avl (title,author)
-      book = collection.detect { |obj| obj[:item][:title] == title} 
-      return book[:item][:availble]
+      book = @collection.detect { |obj| obj[:item][:title] == title && obj[:item][:author] == author } 
+      return book[:available]
     end
 
     def check_out (title,author)
+      avl = check_avl(title,author)
+      if avl == true 
+        index = @collection.find_index { |obj| obj[:item][:title] == title && obj[:item][:author] == author } 
+        @collection[index][:available] = false
+        File.open('./lib/data.yml', 'w') { |f| f.write @collection.to_yaml }
+      end
     end
-
-    
-
-
+     
+    def check_in (title,author)
+      index = @collection.find_index { |obj| obj[:item][:title] == title && obj[:item][:author] == author } 
+      @collection[index][:available] = true
+     File.open('./lib/data.yml', 'w') { |f| f.write @collection.to_yaml }
+    end
 end
--- spec/library_main_spec.rb
+++ spec/library_main_spec.rb
@@ -7,10 +7,31 @@ describe Library_main do
     expect(subject.collection).not_to be nil
     puts subject.collection
   end
+  
+ # it 'check available' do
+ #   expect(subject.check_avl("Pippi Långstrump","Astrid Lindgren") ).to eq true
+ #   expect(subject.check_avl("Osynligt med Alfons","Gunilla Bergström") ).to eq true
+ # end
 
-  it 'check available' do
-    puts subject.check_avl("Alfons och soldatpappan","aa")
-    #expect(subject.check_avl("Alfons och soldatpappan","aa") ).to eq 'true'
-    
+ # it 'check unavailable' do
+ #   expect(subject.check_avl("Skratta lagom! Sa pappa Åberg","Gunilla Bergström") ).to eq false 
+ # end  
+
+  it 'check_out' do
+    if subject.check_avl("Alfons och soldatpappan","Gunilla Bergström") == false 
+      subject.check_in("Alfons och soldatpappan","Gunilla Bergström")
+      expect(subject.check_avl("Alfons och soldatpappan","Gunilla Bergström") ).to eq true
+    else
+      subject.check_out("Alfons och soldatpappan","Gunilla Bergström")
+      expect(subject.check_avl("Alfons och soldatpappan","Gunilla Bergström") ).to eq false
+    end
+    if subject.check_avl("Pippi Långstrump","Astrid Lindgren") == false 
+      subject.check_in("Pippi Långstrump","Astrid Lindgren")
+      expect(subject.check_avl("Pippi Långstrump","Astrid Lindgren") ).to eq true
+    else
+      subject.check_out("Pippi Långstrump","Astrid Lindgren")
+      expect(subject.check_avl("Pippi Långstrump","Astrid Lindgren") ).to eq false
+    end
   end
+
 end
