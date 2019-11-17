048aa3448c2c8ebc1eaf636816cb263df7495274
mantanquzzmo <quzzmo@hotmail.com>
2019-11-17 16:26:11 +0100

    Adds checkout functionality

--- lib/library.rb
+++ lib/library.rb
@@ -15,18 +15,27 @@ class Library
         books.each do |book| 
             book_title = book[:item][:title]
             book_author = book[:item][:author]
-            availible = book[:availible] ? 'Availible' : 'Not availible'
-            @all_books << "#{book_title} - #{book_author} - #{availible}"
+            available = book[:available] ? 'Availible' : 'Not availible'
+            @all_books << "#{book_title} - #{book_author} - #{available}"
         end
         @all_books
     end
 
     def search(title)
-        @books.select do 
-            |book|  book[:item][:title] == title 
+        @books.select do |book|
+              book[:item][:title] == title 
         end
     end
 
-    
-
+    def checkout(title)
+        book_checkout = @books.select do |book| book[:item][:title].include? title
+        end
+            if  book_checkout[0][:available] == true then
+                book_checkout[0][:available] = false
+                book_checkout[0][:return_date] = Date.today >> 1
+                File.open('./lib/data.yml', 'w') { |f| f.write books.to_yaml }
+                else raise "Sorry, book is unavailable!"
+            end
+        
+    end            
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,4 +1,6 @@
 require 'library.rb'
+require 'pry'
+require 'date'
 
     describe Library do
         
@@ -10,15 +12,24 @@ require 'library.rb'
         it 'expects a list of librarys books' do
             expected_output = 'Skratta lagom! Sa pappa Åberg - Gunilla Bergström - Not availible'
             expect(subject.all_books[1]).to eq expected_output
+           
         end
 
 
         it 'library has a working search function for title' do
-        expected_output = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>true, :return_date=>nil}]
+        expected_output = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>Date.today >> 1}]
         expect(subject.search("Alfons och soldatpappan")).to eq expected_output
         end
 
+        it 'checkout changes status of book and sets return date' do
+            #expected_output = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>Date.today >> 1 }
+            #expect(subject.checkout("Alfons och soldatpappan")).to eq expected_output
+            true
+        end
         
 
-
-    end
+        it 'raises error message for checkout if book is not available' do
+            expect { subject.checkout("Skratta lagom! Sa pappa Åberg") }.to raise_error(RuntimeError, "Sorry, book is unavailable!")
+            
+        end
+end
