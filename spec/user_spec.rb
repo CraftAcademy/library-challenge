5bf8a44567fce31264e3d063712e1767d96f9c29
Eevan <eevan.redon@gmail.com>
2019-12-31 11:18:27 +0100

    pull request changes, change user_spec test to books in possession

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,13 +7,10 @@ describe User do
         expect(subject.books_in_posession).to be_empty
     end
 
-    #it 'populates books posession list with newly checked out books' do
-    #    expect(subject.borrowed_books).to eq 
-    #end
-
     let(:library) { Library.new }
     it 'checks out book from library, storing its title, author and return date' do
         expected_output = ["Alfons och soldatpappan - Gunilla Bergström (Return date: #{Date.today.next_month(1).strftime("%Y-%m-%d")})"]
-        expect(subject.checkout_book(library, "Alfons och soldatpappan")).to eq expected_output
+        subject.checkout_book(library, "Alfons och soldatpappan")
+        expect(subject.books_in_posession).to eq expected_output
     end
 end
