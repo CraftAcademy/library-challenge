52bc1e22ec6e6392268359a3b6f5351711d8c6d8
mantanquzzmo <quzzmo@hotmail.com>
2019-12-31 11:20:52 +0100

    updates tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,48 +1,49 @@
+# frozen_string_literal: true
+
 require 'library.rb'
 require 'pry'
 require 'date'
 
- #As a librarian
- #To know what books the library offers
- #I want to be able to see a list of all the library's books
+# As a librarian
+# To know what books the library offers
+# I want to be able to see a list of all the library's books
 
- #As a librarian
- #To know if we have a certain book 
- #I want to be able to search for it by it's title
+# As a librarian
+# To know if we have a certain book
+# I want to be able to search for it by it's title
 
- #As a librarian
- #To help people with the checkout of the books
- #I need to be able to set a new status of the books and set a return date
+# As a librarian
+# To help people with the checkout of the books
+# I need to be able to set a new status of the books and set a return date
 
- #As a librarian
- #To help people with the checkout of the books
- #I want my system to raise an error alerting me if a book is unavailable
+# As a librarian
+# To help people with the checkout of the books
+# I want my system to raise an error alerting me if a book is unavailable
 
-    describe Library do
+describe Library do
+  it 'expects a library to have a list of its books' do
+    expect(subject.books).not_to be nil
+  end
 
-        it 'expects a library to have a list of its books' do
-            expect(subject.books).not_to be nil
-        end
+  before { subject.list_books }
+  it 'expects a list of librarys books' do
+    expected_output = 'Skratta lagom! Sa pappa Åberg - Gunilla Bergström - Not availible'
+    expect(subject.all_books[1]).to eq expected_output
+  end
 
-        before { subject.list_books }
-        it 'expects a list of librarys books' do
-            expected_output = 'Skratta lagom! Sa pappa Åberg - Gunilla Bergström - Not availible'
-            expect(subject.all_books[1]).to eq expected_output 
-        end
+  it 'library has a working search function for title' do
+    expected_output = [{ available: false, item: { title: 'Alfons och soldatpappan', author: 'Gunilla Bergström' }, return_date: Date.today >> 1 }]
+    expect(subject.search('Alfons och soldatpappan')).to eq expected_output
+  end
 
-        it 'library has a working search function for title' do
-            expected_output = [{:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>Date.today >> 1}]
-            expect(subject.search("Alfons och soldatpappan")).to eq expected_output
-        end
+  let(:visitor) { Visitor.new }
+  it 'checkout changes status of book and sets return date' do
+    visitor.return_book('soldat')
+    expected_output = { item: { title: 'Alfons och soldatpappan', author: 'Gunilla Bergström' }, available: false, return_date: Date.today >> 1 }
+    expect { subject.checkout('Alfons och soldatpappan') }.to raise_error(RuntimeError, 'Sorry, book is unavailable!')
+  end
 
-        let(:visitor) { Visitor.new }
-        it 'checkout changes status of book and sets return date' do
-            visitor.return_book("soldat")
-            expected_output = {:item=>{:title=>"Alfons och soldatpappan", :author=>"Gunilla Bergström"}, :available=>false, :return_date=>Date.today >> 1 }
-            expect { subject.checkout("Alfons och soldatpappan") }.to raise_error(RuntimeError, "Sorry, book is unavailable!")  
-        end
-        
-        it 'raises error message for checkout if book is not available' do
-            expect { subject.checkout("Skratta lagom! Sa pappa Åberg") }.to raise_error(RuntimeError, "Sorry, book is unavailable!")   
-        end
+  it 'raises error message for checkout if book is not available' do
+    expect { subject.checkout('Skratta lagom! Sa pappa Åberg') }.to raise_error(RuntimeError, 'Sorry, book is unavailable!')
+  end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,45 +1,43 @@
+# frozen_string_literal: true
+
 require './lib/visitor.rb'
 require 'date'
 require 'pry'
 
- #As a Visitor
- #In order to know which book to borrow
- #I want to the visitor to see a list of books
+# As a Visitor
+# In order to know which book to borrow
+# I want to the visitor to see a list of books
 
- #As a Visitor 
- #In order to check my current book-status with the library
- #I always want to be able to check my App-file
+# As a Visitor
+# In order to check my current book-status with the library
+# I always want to be able to check my App-file
 
-  
- #As a Visitor 
- #To be sure that I have returned a book
- #I want to receive a message upon succesfully returning my book
+# As a Visitor
+# To be sure that I have returned a book
+# I want to receive a message upon succesfully returning my book
 
 describe Visitor do
+  subject { described_class.new }
 
-    subject {described_class.new}
+  it 'is expected not to stash any books on initialize' do
+    expect(subject.borrowed_book).to be nil
+  end
 
-    it 'is expected not to stash any books on initialize' do
-        expect(subject.borrowed_book).to be nil
-    end
+  it 'is expected to see a list of books' do
+    expect(subject.list_books).not_to be nil
+  end
 
-    it 'is expected to see a list of books' do
-        expect(subject.list_books).not_to be nil
-    end
+  let(:library) { Library.new }
+  it 'is expected to be able to return a book' do
+    library.checkout('soldat')
+    expected_output = 'Thanks for returning the book'
+    expect(subject.return_book('soldatpappan')).to eq expected_output
+  end
 
-    let(:library) { Library.new }
-    it 'is expected to be able to return a book' do
-        library.checkout('soldat')
-        expected_output = "Thanks for returning the book"
-        expect(subject.return_book("soldatpappan")).to eq expected_output
-    end
-
-    let(:library) { Library.new }
-    it 'is expected to have a list of borrowed books' do 
-        library.checkout('soldat')
-        expected_output = YAML.load_file('./lib/receipt.yml')
-        expect(subject.borrowed_books).to eq expected_output
-    end
-
-
+  let(:library) { Library.new }
+  it 'is expected to have a list of borrowed books' do
+    library.checkout('soldat')
+    expected_output = YAML.load_file('./lib/receipt.yml')
+    expect(subject.borrowed_books).to eq expected_output
+  end
 end
