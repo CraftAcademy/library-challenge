e71306cc7f4a6c560724159d655c38880d93ba62
Janko <mailodjanka@gmail.com>
2020-02-07 15:42:14 +0100

    Added data.yml which stores the collection of book for the library

--- lib/library.rb
+++ lib/library.rb
@@ -1,17 +1,13 @@
+require 'yaml'
+
 class Library 
     attr_accessor :collection
 
     STANDARD_LENDING_PERIOD_IN_DAYS = 30
 
-    def initialize
-        collection = []
-    end
+    def collection
+        collection = YAML.load_file('./lib/data.yml')
 
-
-    def addBook(title, author)
-        return_date = Time.now + STANDARD_LENDING_PERIOD_IN_DAYS
-        book = {item: {title: title, author: author}, available: true, return_date: return_date}
-        collection.push(book)
     end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,13 +6,12 @@ require './lib/library.rb'
 
 describe Library do
     
-    it 'Finds the library empty' do
-        expect(subject.collection).to eq nil
-    end
+    #it 'Finds the library empty' do
+        #expect(subject.collection).to eq nil
+    #end
 
     it 'finds if the library has books in stock' do
-        subject.addBook('Hejsan', 'Janko')
-        expect(library).to_not eq nil
+        expect(collection).to_not eq nil
     end
 
 end
