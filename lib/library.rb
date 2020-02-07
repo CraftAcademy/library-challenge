714010c7eba1e5219c461a8c296018911bf66267
Anish2504 <anish.kanswal@gmail.com>
2020-02-07 21:44:55 +0100

    create add book test

--- lib/library.rb
+++ lib/library.rb
@@ -2,10 +2,21 @@ require 'yaml'
 #require 'date'
 
 class Library
-    attr_accessor :catalog
+    attr_accessor :catalog, :title, :author, :available
 
-    def initialize
+def initialize
         @catalog = YAML.load_file('./lib/data.yml')
-    end
+end
+
+
+def new_book(args)
+        title ≈ args[ :title] 
+        author ≈ args[ :author]
+        available = true
+end
+   
+def add_book
+    File.open('./lib/data.yml', 'w') { |f| f.write new_book.to_yaml }
+end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,9 +2,15 @@ require 'yaml'
 require './lib/library.rb' #links to this file so the tests can refer to this code
 
 describe Library do 
-
-    #it 'add new books' do
-    #end
+ { instance_double( title: 'Lord of the rings', author: 'JRR Tolkien ' , available: True) }
+      
+#before { subject.add_book}
+        
+it "add new books" do
+    expected_output = 'Lord of the rings'
+    expect(subject.catelog.detect { |obj| obj[:item][:title] == "Lord of the rings"  }).to eq expected_output
+    end
+end
 
     #it 'delete old books' do
     #end
@@ -22,6 +28,8 @@ describe Library do
         expected_output = YAML.load_file('./lib/data.yml')
         expect(subject.catalog).to eq expected_output
     end
+
+
 
 end
 
