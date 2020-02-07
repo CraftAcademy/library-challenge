3cdf5ef4a8ea20f750dbe2b3e2a1c890fa52d4e9
Anish2504 <anish.kanswal@gmail.com>
2020-02-07 15:54:36 +0100

    create test for library

---
+++ spec/library_spec.rb
@@ -0,0 +1,27 @@
+require './lib/library.rb'
+
+describe Library do 
+    it 'add new books' do
+    end
+
+    it 'delete old books' do
+    end
+
+    it 'search for books by title' do
+    end
+
+    it 'search for books by author' do
+    end
+
+    it 'books status shown' do
+    end
+
+    it 'view all books' do
+    end
+
+end
+
+
+
+
+
