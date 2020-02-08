f64d626e9ec0ab62e4702dd09aef903cfb6a1bec
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-08 23:48:12 +0100

    added search test

--- lib/library.rb
+++ lib/library.rb
@@ -1,12 +1,27 @@
 require 'yaml'
 
 class Library
-
     attr_accessor :books_list
+    
 
-    def initialize
-
-    @books_list = YAML.load_file('./lib/data.yml')   
-
+    def books_list
+        books_list = YAML.load_file('./lib/data.yml')   
     end
+
+
+
+    def search_by_title(title)
+        books_list.select { |book| book[:item][:title].include? title }
+    end
+
+    def search_by_author(author)
+        books_list.select { |book| book[:item][:author].include? author }
+    end
+
+    def available_books
+        books_list.select { |book| book[:available].eq? true }
+    end
+
+
+    
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,15 +2,28 @@ require './lib/library.rb'
 
 describe Library do
 
+        #let(:book) {instance_double ('book',book_name: 'Pippi Långstrump')}
    
 
     it 'returns true if there is a list of books' do
         expect(subject.books_list).not_to be nil
     end
 
-    it 'returns true or false depending on book availability ' do
+    it 'Can search for book by title' do
+        expected_book = [{item: {title: 'Pippi Långstrump går ombord',author: 'Astrid Lindgren'}, available: true, return_date: nil }]
+        expect(subject.search_by_title('går ombord')).to eq(expected_book)
+    end
 
+    it 'Can search for book by author' do
+        expected_book = [{item: {title: "Pippi Långstrump", author: "Astrid Lindgren"}, available: true, return_date: nil}, 
+        {item: {title: "Pippi Långstrump går ombord", author: "Astrid Lindgren"}, available: true, return_date: nil}]
+        expect(subject.search_by_author('Astrid Lindgren')).to eq(expected_book)
+    end
 
+    it 'check books that are available' do
+        expect(subject.available_books).to eq [{available: true}]
+        
+    end
 
 
 end
