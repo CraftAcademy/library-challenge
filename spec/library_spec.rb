a4a49eab5b823f5ed6324a4b6ac912a886ecbf61
Blake <blake@futchi.com>
2020-02-08 16:19:54 +0100

    added it statements for tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,40 @@
 require './lib/library.rb'
+
+describe Library do
+  
+  it 'has a list of books' do
+    
+  end
+
+  it 'can search book list by author' do
+    
+  end
+
+  it 'can search book list by title' do
+    
+  end
+
+  it 'can check if the book is available' do
+    
+  end
+
+  it 'can generate checkout date' do
+    
+  end
+
+  it 'can generate return date' do
+    
+  end
+
+  it 'when book checked out availability changes to unavailable' do
+    
+  end
+
+  it 'when book returned availability changes to available' do
+    
+  end
+
+  it 'when checkout occurs, visitor pinged return date information' do
+    
+  end
+end
