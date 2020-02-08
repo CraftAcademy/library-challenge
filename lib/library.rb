26bba05a0b605596a2da0d0e409c80ffb245e02f
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-08 14:02:41 +0100

    pass tests for book to check out, for user message for sucessful checkout and return date

--- lib/library.rb
+++ lib/library.rb
@@ -1,17 +1,16 @@
 require 'yaml'
-#require 'date'
+require 'date'
 
 class Library
-    attr_accessor :catalog, :title, :author, :availability, :book, :return_date
+    attr_accessor :catalog, :return_date, :books_checked_out
 
-    STANDARD_TIME_DAY = 30
+    STANDARD_TIME_MONTH = 1
 
     def initialize
         @catalog = YAML.load_file('./lib/data.yml')
         @return_date = set_return_date()
- 
     end
-
+#User can search
     def author_search(user_search)
         @book = catalog.select { |obj| obj[:item][:author].include? user_search }
     end
@@ -20,19 +19,35 @@ class Library
         @book = catalog.select { |obj| obj[:item][:title].include? user_search }
     end
 
-    def set_return_date
-        Date.today.next_day(STANDARD_TIME_DAY).strftime('%D')
-    end
-
-    #def checkout(user_search)
-    def book_availability?
-        find.book_to_checkout[:available] == false ? 'checkout incomplete,book unavailable' : checkout()
-
-        end
     
+    #We want to first find the book the user wants to check out
     def book_to_checkout(user_search)
-        catalog.find { |obj| obj[:title] == user_search }
+        books_checked_out = []
+        books_checked_out << @catalog.detect { |obj| obj[:item][:title] == user_search }
     end
+    
+    # we then want to check the book's availability
+    # we then want an OR statement to either checkout book or say book is not available (maybe do in one step?)
+    def book_availability?(user_search)
+        availablity = book_to_checkout(user_search)[0][:available]
+        availablity ? checkout(user_search) : 'Checkout incomplete, book unavailable.'
+    end
+    
+    # then if the book is available we want to 'checkout' by updating the books availability and return_date and message to user
+    def checkout(user_search)
+        #update availability
+        book_to_checkout(user_search)[0][:available] == false
+        #update return date
+        book_to_checkout(user_search)[0][:return_date] = set_return_date
+        #message to user
+        { message: 'Checkout complete', return_date: set_return_date } 
+    end
+    
+    def set_return_date
+        Date.today.next_month(STANDARD_TIME_MONTH).strftime('%D')
+    end   
+      
+
 =begin
     def checkout(message, return_date)
         catalog.
