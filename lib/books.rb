0df62e46c41e4af19039bb1b1316b797f5476c9d
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-08 16:56:02 +0100

    stuck on books class

--- lib/books.rb
+++ lib/books.rb
@@ -1,29 +1,19 @@
 require 'yaml'
 books = YAML.load_file('./lib/data.yml')
+puts books
+books = [item1:]
+books.each do |title|
+puts "#{title[:title]}" end
 
 
-class Books
-    attr_accessor :item
+# class Books
+#     attr_accessor :item
 
-    def initialize
-      @item = YAML.load_file('./lib/data.yml')
-    end
-end
+#     def initialize
+#       @item = [YAML.load_file('./lib/data.yml')]
+#     end
+# end
 
 
 
-# attr_accessor :title, :author, :available, :return_date 
 
-#attr_accessor :books 
-
-  #  def self.perform_action! specific_name
-   #     init = YAML.load_file('./lib/data.yml')[specific_name]
-   #     input = init[:item]
-    #    result = eval(input)
-   # end
-
-   # def initialize
-     #   @books = 'Books'
-  #  end
-
-#YAML.load_file(./lib/data.yml)
