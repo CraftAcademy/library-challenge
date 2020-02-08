cb0df08e9e0182722f6c3a181783b4daf08118c7
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-08 09:55:23 +0100

    adding tests to search for a specific book but now with the whole title, and passing that

--- lib/library.rb
+++ lib/library.rb
@@ -1,13 +1,21 @@
 require 'yaml'
 
 class Library
-   attr_accessor :collection
+   attr_accessor :collection, :search_word
 
    DAYS_BEFORE_RETURN = 30
+
+   def initialize
+      @search_word = nil
+   end
 
    def collection
     collection = YAML.load_file('./lib/inventory.yml')
    end 
+
+   # def select(search_word)
+   #    collection.select {|book| book[:item][:title].include? search_word}
+   # end
     
 end 
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -8,7 +8,16 @@ describe Library do
         expect(subject.collection).to_not eq nil
     end 
 
-    it 'to search for a specific book' do
+    it 'to search for a specific book by author' do
         expect(subject.collection[2][:item][:author]).to eq 'Fatima Bremmer'
     end
-end 
+
+    it 'to search for a specific book by title' do 
+        expect(subject.collection[1][:item][:title]).to eq "Bränn alla mina brev"
+    end
+    
+    it 'to search for a book with only part of the title' do
+        expected_output = [{:item=>{:title=>"In search of lost time", :author=>"Marcel Proust"}, :available=>true, :return_date=>nil}] 
+        expect(subject.collection.select {|book| book[:item][:title].include? "of lost"}).to eq expected_output
+    end
+end
