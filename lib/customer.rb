4569705d9fee051dcb353178f7dd498d3464104e
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-09 14:40:02 +0100

    passing in the funtionality of the module to the customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,8 @@
+require 'library_service'
+
 class Customer
 
+    def checkout
+        LibraryService.checkout_service(book)
+    end 
 end
--- lib/library_service.rb
+++ lib/library_service.rb
@@ -3,13 +3,13 @@ require 'customer'
 
 module Libraryservice
     
-    def checkout(book)
+    def checkout_service(book)
         collection[book][:available]= false 
         collection[book][:return_date]= set_return_date()
         File.open('./lib/inventory.yml', "w") {|f| f.write collection.to.yaml}
     end 
 
-    def checkin(book)
+    def checkin_service(book)
         collection[book][:available]= true 
         collection[book][:return_date]= nil
         File.open('./lib/inventory.yml', "w") {|f| f.write collection.to.yaml}
