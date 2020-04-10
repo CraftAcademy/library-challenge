c709fc130950f3d5a28914a056bdf8867ac8ee5d
allezz <aerbay@gmail.com>
2020-04-10 21:25:43 +0200

    Add test for book author,title,category[passed]

--- lib/book.rb
+++ lib/book.rb
@@ -3,9 +3,9 @@ class Book
     attr_accessor :title, :author, :category, :available, :loanee, :return_date
     DEFAULT_LOAN_DURATION_DAYS = 30
     def initialize(attrs = {})
-        @title = attrs[:title]
-        @author = attrs[:author]
-        @category = attrs[:category]
+        @title = set_title(attrs[:title])
+        @author = set_author(attrs[:author])
+        @category = set_category(attrs[:category])
         @available = true
         @loanee 
         @return_date
@@ -21,6 +21,32 @@ class Book
         @loanee = nil
         @return_date = nil
         @available = true        
+    end
+
+    private
+
+    def set_title(title)
+        title == nil ? missing_title : title
+    end
+
+    def set_author(author)
+        author == nil ? missing_author : author
+    end
+
+    def set_category(category)
+        category == nil ? missing_category : category
+    end
+
+    def missing_author
+        raise "Book must have an author"
+    end
+
+    def missing_title
+        raise "Book must have a title"
+    end
+
+    def missing_category
+        raise "Book must have a category"
     end
 
 end
