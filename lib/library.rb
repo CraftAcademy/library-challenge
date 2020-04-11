449b3a4166320c7117fb8f5dfe27b2b6384f1c26
japh <jenny.scherr@svenskaspel.se>
2020-04-11 14:05:59 +0200

    added view collection and search title of book

--- lib/library.rb
+++ lib/library.rb
@@ -1,19 +1,27 @@
 class Library
-    attr_accessor :collection
+  attr_accessor :collection
 
-    def initialize
-        collection_present
-        @collection = load_collection
-    end
-    def collection_present
-       if YAML.load_file('./lib/data.yml')
-       return true 
-       end
-    end
-    def load_collection
-        YAML.load_file('./lib/data.yml')
-    end
-    def search(title)
-        collection.select { |obj| obj[:item][:title].include?(title) }
-    end
+  def initialize
+    @collection = load_collection
+  end
+  # def collection_present
+  #   if YAML.load_file('./lib/data.yml')
+  #    return true 
+  #   end
+  # end
+
+  def load_collection
+    collection = YAML.load_file('./lib/data.yml')
+    collection.length.nil? ? 'No books in the library' : collection
+  end
+  
+  def view_collection
+    @collection.each {|key, value| puts "#{key[:item][:title]} : #{key[:item][:author]}"}
+  end
+
+  def search(search_title)
+    title = @collection.select { |obj| obj[:item][:title].include?search_title }
+    title[0]
+  end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,49 +3,18 @@ require 'date'
 require 'yaml'
 
 describe Library do
-#   let(:account) { instance_double('Account', card:, collection: ) } 
+  subject { described_class.new()}
+  it 'has collection on initialize' do
+    expect(subject.collection).to be_truthy
+  end
+  
+  it 'be able to view collection' do
+    subject.view_collection
+  end 
 
-    it 'has collection on initialize' do
-      expect(subject.collection_present).to eq true   
-    end
-    it 'searches for a book title in collection' do
-      subject.search(title: 'Pippi Långstrump') 
-      expect(subject.search).to eq 'Pippi Långstrump'
-    end
+  it 'searches for a book title in collection' do
+    expect(subject.search('Pippi Långstrump') ).to include(available: true)
+  end
 
  end   
-    # it 'allows withdrawal if the account has enough balance.' do
-    #   expected_output = { 
-    #     status: true,
-    #     message: 'success',
-    #     date: Date.today, 
-    #     amount: 45,
-    #     bills: [20, 20, 5]}
-    #   expect(subject.withdraw(45, '1234', account, :exp_date, :account_status)).to eq expected_output
-    # end
 
-    # it 'rejects withdrawal if account has insufficient funds' do
-    #   expected_output = { status: false, message: 'insufficient funds in account', date: Date.today }
-    #   expect(subject.withdraw(1005, '1234', account, :exp_date, :account_status)).to eq expected_output
-    # end
-    # it 'rejects withdrawal if ATM has insufficient funds' do
-    #   subject.funds = 50
-    #   expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    #   expect(subject.withdraw(100, '1234', account, :exp_date, :account_status)).to eq expected_output
-    # end
-    # it 'rejects withdrawal if the pin is wrong' do
-    #   expected_output = { status: false, message: 'wrong pin', date: Date.today}
-    #   expect(subject.withdraw(50, 9999, account, :exp_date, :account_status)).to eq expected_output
-    # end
-    # it 'rejects withdrawal if the card is expired' do
-    #   allow(account).to receive(:exp_date).and_return('12/15')
-    #   expected_output = { status: false, message: 'card expired', date: Date.today }
-    #   expect(subject.withdraw(6, '1234', account, :exp_date, :account_status)).to eq expected_output
-    # end 
-    # it 'rejects withdrawal if the account is disabled' do
-    #   allow(account).to receive(:account_status).and_return(:disabled)
-    #   expected_output = { status: false, message: 'account is disabled', date: Date.today }
-    #   expect(subject.withdraw(6, '1234', account, :exp_date, :account_status)).to eq expected_output
-    # end
-    
-# end
