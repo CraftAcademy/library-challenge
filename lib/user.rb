c644e24d65f9212420ec5bfb69079892a82ef032
japh <jenny.scherr@svenskaspel.se>
2020-04-12 14:42:45 +0200

    added search book method and test as a user

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,14 @@
+require './lib/library.rb'
 class User 
-    attr_accessor :name, :account
+    attr_accessor :name, :account, :library
 
     def initialize(attr={})
         @name = :name
-        
+        @library = Library.new
+        @book = nil
     end
 
+    def search_book(search_word)
+      @book = @library.search(search_word)
+    end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -11,6 +11,15 @@ describe User do
         expect(subject.name).not_to be nil
     end
     
+    it 'searches for a book title in collection' do
+      expect(subject.search_book('Pippi Långstrump') ).to include(include(:available))
+    end
+    it 'searches for a book author in collection' do
+      expect(subject.search_book('Astrid Lindgren') ).to include(include(:available))
+    end
+    it 'searches for a book author not in the collection' do
+      expect(subject.search_book('Strindberg') ).to eq 'no such book'
+    end
     # it ' is expected to raise an error if no name is set' do
     #     expect { described_class.new }.to raise_error 'A name is required'
     # end
