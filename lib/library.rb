f7b412175c174dbd371ef5cf73c72bf10c92b24c
odeane_croney <odeane_croney@yahoo.com>
2020-06-13 19:55:02 +0200

    testing for book title, and calling the title

--- lib/library.rb
+++ lib/library.rb
@@ -1,11 +1,18 @@
 require 'yaml'
 
 class Library
-    attr_accessor :catalogue
+    attr_accessor :catalogue 
 
     def initialize
         @catalogue = YAML.load_file('./lib/data.yml')
-        
+
     end
+
+    def show_title
+    book_name = @catalogue[0][:item][:title]
+    end
+
+
+
 end
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,11 +5,18 @@ require'date'
 
 describe Library do
     
-it 'check for available book catalogue' do
+it 'check if book catalogue exist' do
     expect(subject.catalogue).to_not eq nil 
 end
 
+  it 'show list of books with title' do
+      expected_output = 'Alfons och soldatpappan'
+      expect(subject.show_title).to eq expected_output
+  end
 
+#   it 'check if the book is unavailable' do
+#       expect(subject.book_status).to eq :available
+#   end
 
 end
 
