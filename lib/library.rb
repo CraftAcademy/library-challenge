1451de081e3aef848851cd6ddb72e4d790373c81
odeane_croney <odeane_croney@yahoo.com>
2020-06-13 16:34:31 +0200

    writing a test to check for number of available items and and initializing this value

--- lib/library.rb
+++ lib/library.rb
@@ -1,3 +1,10 @@
+require 'yaml'
+
 class Library
-    
+    attr_accessor :catalogue
+
+    def initialize
+        @catalogue = 5
+    end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,7 +2,14 @@ require "./lib/library.rb"
 require'date'
 
 
+
 describe Library do
     
+it 'has 5 books on initialization' do
+    expect(subject.catalogue).to eq 5
+end
+
+
+
 end
 
