e5720655c37ff815b802a1aa868cac7dce072b20
odeane_croney <odeane_croney@yahoo.com>
2020-06-13 20:41:32 +0200

    Setting and testing for book validity period

--- lib/library.rb
+++ lib/library.rb
@@ -1,18 +1,32 @@
 require 'yaml'
+require 'date'
 
 class Library
-    attr_accessor :catalogue 
+    attr_accessor :catalogue, :book_status, :exp_date
+
+    STANDARD_VALIDITY_DAYS = 30
 
     def initialize
         @catalogue = YAML.load_file('./lib/data.yml')
-
+        @exp_date = return_date
     end
 
+   def book_list
+        @catalogue
+   end
+   
     def show_title
     book_name = @catalogue[0][:item][:title]
     end
 
+     def book_status
+      @book_status = :available   
+     end
 
+     def return_date
+         Date.today.next_day(STANDARD_VALIDITY_DAYS).strftime('%d/%m')
+     end
+    
 
 end
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,14 +9,24 @@ it 'check if book catalogue exist' do
     expect(subject.catalogue).to_not eq nil 
 end
 
-  it 'show list of books with title' do
+  
+it 'show book list' do
+    expect(subject.book_list).to_not eq nil
+end
+
+it 'show list of books with title' do
       expected_output = 'Alfons och soldatpappan'
       expect(subject.show_title).to eq expected_output
   end
 
-#   it 'check if the book is unavailable' do
-#       expect(subject.book_status).to eq :available
-#   end
+  it 'check if the book is unavailable' do
+      expect(subject.book_status).to eq :available
+  end
+
+  it 'expected to have an return date on initialize' do
+      expected_date = Date.today.next_day(30).strftime('%d/%m')
+      expect(subject.return_date).to eq expected_date
+  end
 
 end
 
