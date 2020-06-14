2180055b04d0cf94ff101752dff975bdaa0d79d7
Sima <sima.boreiri@gmail.com>
2020-06-14 16:01:43 +0200

    write show list test and pass

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,4 +1,16 @@
 require 'yaml'
 require 'date'
+require './lib/library.rb'
+
 class Visitor 
+
+    def initialize 
+       @catalogue = YAML.load_file('./lib/data.yml')
+    end
+
+    def show_list 
+        @catalogue 
+
+    end
+
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,5 +1,8 @@
 require './lib/visitor.rb'
 require 'date'
+
 describe Visitor do
-    
+ it 'visitor should see the book list' do 
+    expect(subject.show_list).to_not eq nil
+ end   
 end
