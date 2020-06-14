76474ea1cd6696cfd99e5d34c3439e17325677f9
Sima <sima.boreiri@gmail.com>
2020-06-14 16:28:12 +0200

    write test for visitor to see the book title and pass it

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -3,6 +3,7 @@ require 'date'
 require './lib/library.rb'
 
 class Visitor 
+    
 
     def initialize 
        @catalogue = YAML.load_file('./lib/data.yml')
@@ -10,7 +11,15 @@ class Visitor
 
     def show_list 
         @catalogue 
-
     end
+
+   def see_title 
+     book_title = [@catalogue[0][:item][:title],
+                     @catalogue[1][:item][:title],
+                     @catalogue[2][:item][:title],
+                     @catalogue[3][:item][:title],
+                     @catalogue[4][:item][:title]] 
+    end
+
 
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -4,5 +4,13 @@ require 'date'
 describe Visitor do
  it 'visitor should see the book list' do 
     expect(subject.show_list).to_not eq nil
- end   
+ end  
+
+ it 'visitor should see books title' do 
+    expect(subject.see_title).to_not eq nil 
+ end
+
+
+
+ 
 end
