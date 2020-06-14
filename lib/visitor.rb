c6313d1bce96b3c22db56ca0930c103ee3d21d79
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-14 18:26:27 +0200

    Deleting unecessary files and added implementations and tests to visitor spec and file. Allowing visitor to see their list of checked out books. Tests passing.

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,7 +1,16 @@
-require './lib/data.yml'
+require 'yaml'
 require './lib/library.rb'
-require './lib/'
+require './lib/visitor.rb'
 
-class Visitor do
-    attr_accessor :booklist, 
+class Visitor
+    attr_accessor :my_books, :collection
+
+    def initialize
+        @collection = YAML.load_file('./lib/data.yml')
+        @my_books = Library.new
+    end
+
+    def my_list
+        my_list = @my_books.book_checkout
+    end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,8 +1,17 @@
-require './lib/data.yml'
+require 'yaml'
 require './lib/visitor.rb'
+require './lib/library.rb'
 require 'date'
 
 describe Visitor do
     let(:person) {instance_double('Person', name: 'Viktor')}
-    subject { described_class.new({owner: person}) }
+
+    it 'have a collection on initialize' do
+        expected_output = YAML.load_file('./lib/data.yml')
+        expect(subject.collection).to eq expected_output
+    end
+
+    it 'shows me a list of books in my possession' do
+        expect(subject.my_list).to_not include(nil)
+    end
 end
