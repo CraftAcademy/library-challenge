c9311c73da80597487345bce6c0ac607ce8eb785
Mauro  Avellaneda <mauroeavellaneda@gmail.com>
2020-08-29 18:47:58 +0200

    creating a test to check if a certain book is available or not, with test passed

--- lib/database.rb
+++ lib/database.rb
@@ -11,10 +11,29 @@ class Database
 
   def items_by_title
     items = [ books[0][:item][:title], 
-              books[0][:item][:title], 
-              books[0][:item][:title], 
-              books[0][:item][:title], 
-              books[0][:item][:title] ]
+              books[1][:item][:title], 
+              books[2][:item][:title], 
+              books[3][:item][:title], 
+              books[4][:item][:title] 
+              ]
+  end
+
+  def items_by_author
+    items = [ books[0][:item][:author], 
+              books[1][:item][:author], 
+              books[2][:item][:author],
+              books[3][:item][:author],
+              books[4][:item][:author]
+              ]
+  end
+
+  def book_available
+    items = [ books[0][:item][:available], 
+              books[1][:item][:available], 
+              books[2][:item][:available],
+              books[3][:item][:available],
+              books[4][:item][:available] 
+            ]
   end
 
   
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -3,6 +3,7 @@ require 'yaml'
 require 'pry'
 
 
+
 #In order for the customers to know what books are available to get borrowed, 
 #we need to create a database for them to interact with. 
 
@@ -17,15 +18,24 @@ describe Database do
   end
 
   #In order to search books using the title as a reference,
-  #we need to create a function that allow us to list the authors
+  #we need to create a function that allow us to list the titles
 
   it 'shows the books that are available by their title' do
-    
     expect(subject.items_by_title).to_not eq nil
-
-
   end
-  
+    #In order to search books using the author as a reference,
+  #we need to create a function that allow us to list the authors
+
+  it 'shows the books that are available by their author' do
+    expect(subject.items_by_author).to_not eq nil
+  end
+
+  #In order to check the availability of a certain book,
+  #we need to create a function that tell us which book is in the library
+
+  it 'shows the availability of a certain book' do 
+    expect(subject.book_available).to_not eq false
+  end
 
 
 
