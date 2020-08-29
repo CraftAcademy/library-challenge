7acbe7189a8c198c9816668f2eb6e0544df7da83
Mauro  Avellaneda <mauroeavellaneda@gmail.com>
2020-08-29 18:06:43 +0200

    adding second test of 'shows the books that are available by their title'. The test is passing

--- lib/database.rb
+++ lib/database.rb
@@ -1,10 +1,30 @@
+require 'yaml'
+require 'pry'
+
 class Database
 
   attr_accessor :books
 
   def initialize 
-    @books = 10
+    @books = YAML.load_file('./lib/data.yml')
   end
+
+  def items_by_title
+    items = [ books[0][:item][:title], 
+              books[0][:item][:title], 
+              books[0][:item][:title], 
+              books[0][:item][:title], 
+              books[0][:item][:title] ]
+  end
+
+  
+    
+
+    
+
+
+
+  
 
 
 
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,15 +1,33 @@
-#In order to the customers know what books are available to get borrowed, we need to create a database for them to interact with. 
 require './lib/database.rb'
+require 'yaml'
 require 'pry'
+
+
+#In order for the customers to know what books are available to get borrowed, 
+#we need to create a database for them to interact with. 
+
 
 
 describe Database do
 
   it 'has books availables' do
-    expect(subject.books).to eq 10 
+    @books = YAML.load_file('./lib/data.yml')
+    expect(subject.books).to eq @books
     
   end
+
+  #In order to search books using the title as a reference,
+  #we need to create a function that allow us to list the authors
+
+  it 'shows the books that are available by their title' do
+    
+    expect(subject.items_by_title).to_not eq nil
+
+
+  end
   
+
+
 
 
 
