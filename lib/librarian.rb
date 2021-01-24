a788584cf438d335e0fb93fabca4ef532255a1db
FoxyTony <mirantony@icloud.com>
2021-01-24 10:37:10 +0100

    Librarian has a list of the books

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -1,9 +1,15 @@
 require 'date'
+require 'yaml'
 
 class Librarian
+    attr_accessor :list
 
     def initialize
-
+        @list = YAML.load_file('./lib/data.yml')
+        
     end
+
+    def check_availability(title)
+        list
     
 end
--- spec/librarian_spec.rb
+++ spec/librarian_spec.rb
@@ -2,7 +2,14 @@ require './lib/librarian.rb'
 require 'date'
 
 describe Librarian do
-    
+
+it 'librarian checks if he has a list of the books' do
+expect(subject.list).not_to be nil
+end
+
+it 'librarian can check the availability' do
+expect(subject.check_availability('Big Fish')).to be true
+end
     
     
 end
