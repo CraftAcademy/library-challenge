515bc272915ec30e0f21eeb4752ce065ce4871d4
arunbhalli <tomarbhalli.com>
2021-04-03 20:38:05 +0200

    Book list update in file dtat,yml

--- lib/keeper.rb
+++ lib/keeper.rb
@@ -9,5 +9,8 @@ class Keeper
     def initialize
         @list = YAML.load_file('./lib/data.yml')
     end
-
+    def check_availability(title)
+        book = @list.detect { |hash| hash[:book][:title] == title }
+        book[:available]
+    end
 end
--- spec/keeper_spec.rb
+++ spec/keeper_spec.rb
@@ -4,5 +4,8 @@ describe Keeper do
     it 'keeeper checks if he has a list of the books' do
         expect(subject.list).not_to be nil
     end
-  
+    it 'keeeper can check the availability of a specific title' do
+            expect(subject.check_availability('Big Fish')).to be true
+    end
+    
 end
