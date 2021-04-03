179bb43aefe036dd6ed7f6286d4d27f6c0e38565
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-03 11:37:50 +0200

    Updating read_book_list functionality, add require yaml

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,14 @@
+require 'yaml'
+
 class Library 
+    attr_accessor :book_list
 
-  def initialize
-    @book_list
-  end
+    def initialize
+        @book_list
+    end
 
-  def read_book_list
-    YAML.load_file('./lib/books.yml')
-  end
+    def read_book_list
+        @book_list = YAML.load_file('./lib/books.yml')
+    end
+
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,7 +2,16 @@ require './lib/library.rb'
 require 'date'
 
 describe Library do
-  it 'reads book.yml as an object' do
-    expect(subject.read_book_list).not_to be nil
-  end
+  
+    it 'checks if on initialize book_list var is nil' do
+        expect(subject.book_list).to be nil
+    end
+
+    it 'reads book.yml as an object' do
+        subject.read_book_list
+        expect(subject.book_list).not_to be nil
+    end
+
+    
+
 end
