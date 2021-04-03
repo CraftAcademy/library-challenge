2150c8a08ad9d13a5bb6c3d78cc18e68c36c32f0
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-03 12:37:51 +0200

    Add pull_book method

--- lib/library.rb
+++ lib/library.rb
@@ -1,14 +1,31 @@
 require 'yaml'
+require 'pry'
 
 class Library 
-    attr_accessor :book_list
+    attr_accessor :book_list, :pulled_book
 
     def initialize
         @book_list
+        @book
     end
 
     def read_book_list
         @book_list = YAML.load_file('./lib/books.yml')
+    end
+
+    def pull_book(args = {})       
+        args[:title] == nil ? title_missing_error : return_book(args[:title])
+    end
+    
+    private
+
+    def title_missing_error
+        #binding.pry
+        raise RuntimeError, "Specify the title of book to pull"
+    end
+
+    def return_book(title)
+        @pulled_book = @book_list.detect { |book| book[:item][:title] == title }
     end
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -12,6 +12,15 @@ describe Library do
         expect(subject.book_list).not_to be nil
     end
 
-    
+    it 'can pull individual book from book_list' do
+        subject.read_book_list
+        expected_outcome = {:item=>{:title=>"Foundation", :author=>"Isaak Asimov"}, :avalible=>false, :return_date=>"2021-05-03"}
+        expect(subject.pull_book({title: 'Foundation'})).to eq expected_outcome
+    end
+
+    it 'returns error if no title is given to pull_book method' do
+        subject.read_book_list        
+        expect{ subject.pull_book }.to raise_error(RuntimeError, "Specify the title of book to pull")
+    end
 
 end
