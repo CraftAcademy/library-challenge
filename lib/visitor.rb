ea4b426e029a5d0fe9626bc2d247d0d7d942dd3e
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-04 17:30:29 +0200

    Add variable  with currently owned books

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -2,8 +2,13 @@ require 'pry'
 require './lib/library.rb'
 
 class Visitor < Library    
-    attr_accessor :receipt
-
+    attr_accessor :receipt, :books_in_possesion
+    
+    def initialize
+        @books_in_possesion = Array.new
+    end
+    
+    
     def see_available                     
         all_books = self.read_book_list
         availible_books = all_books.select { |book| book[:available] == true }             
@@ -22,7 +27,8 @@ class Visitor < Library
     
     def give_receipt
         self.change_status
-        self.save_book_list
+        self.save_book_list        
+        @books_in_possesion << {title: @pulled_book[:item][:title], return_date: @pulled_book[:return_date]}        
         @receipt = {title: @pulled_book[:item][:title], return_date: @pulled_book[:return_date], date: Date.today.strftime('%d-%m-%y') }
     end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,7 +3,7 @@ require './lib/library.rb'
 require 'date'
 
 describe Visitor do
-
+    expected_return_date = Date.today.next_day(30).strftime('%d-%m-%y')
     
 
     it 'can check what books are available for rent' do
@@ -20,24 +20,37 @@ describe Visitor do
         subject.rent_the_book({title: 'Lord of the Rings'})
         subject.read_book_list
         subject.pull_book({title: 'Lord of the Rings'})
-        expected_outcome = {:item=>{:title=>"Lord of the Rings", :author=>"J.R.R. Tolkien"}, :available=>false, :return_date=>subject.thirty_days_later}
+        expected_outcome = {:item=>{:title=>"Lord of the Rings", :author=>"J.R.R. Tolkien"}, :available=>false, :return_date=>expected_return_date}
         expect(subject.pulled_book).to eq expected_outcome
     end
 
     it 'sends a recipet' do
         subject.read_book_list
         subject.rent_the_book({title: 'Fifty Shades of Grey'})
-        expected_outcome = {title: "Fifty Shades of Grey", return_date: subject.thirty_days_later, date: Date.today.strftime('%d-%m-%y') }
+        expected_outcome = {title: "Fifty Shades of Grey", return_date: expected_return_date, date: Date.today.strftime('%d-%m-%y') }
         expect(subject.receipt).to eq expected_outcome
     end  
+
+    it 'checks for the list of currently owned books' do
+        subject.read_book_list
+        subject.pull_book({title: 'Lord of the Rings'})
+        subject.change_status
+        subject.rent_the_book({title: 'Lord of the Rings'})
+        subject.rent_the_book({title: 'Halo: Fall of Reach'})
+        expected_outcome = [
+            {title: 'Lord of the Rings', :return_date=>expected_return_date},
+            {title: 'Halo: Fall of Reach', :return_date=>expected_return_date}
+        ]
+        expect(subject.books_in_possesion).to eq expected_outcome
+    end
 
     after(:all) do
         book_list = YAML.load_file('./lib/books_original_state.yml')
         File.open('./lib/books.yml', 'w') { |file| file.write book_list.to_yaml }
       end
     
-    #rent_the_book
-    #see_currently_owned
+    
+    
     #return_the_book
 
 end
