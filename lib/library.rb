7edb0eee3fb672ce248ffb014f68a0f6c0ed253c
Christer <christerforsgren91@gmail.com>
2021-04-04 20:23:51 +0200

    added test for book list and book search, plus implementation method

--- lib/library.rb
+++ lib/library.rb
@@ -1,8 +1,13 @@
 
 
 class Library 
-
+    attr_accessor :books 
     def initialize 
+        @books = YAML.load_file('./lib/data_test.yml')
+    end
+
+    def book_search(input)
+        books.select {|books| books[:book][:title].include? input}
     end
     
 end
