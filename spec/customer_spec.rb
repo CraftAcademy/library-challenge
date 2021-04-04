179790f8b6d961633a18501ef3134de2c864290c
DavDan1 <david-danielyan@hotmail.com>
2021-04-04 19:01:14 +0200

    added method `before`

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -9,7 +9,28 @@ describe Customer do
     let:(:keeper){instance_double}('Keeper', books: [])
     subject{Customer.new(name)}
 
-   
+    before do
+        allow(keeper).to receive(check_out)
+    end
+
+    it 'is expected to have a name on start' do
+        expect(subject.name).to eq name
+    end
+
+    it 'is expected to have a list of books on start' do
+        expect(subject.books).to eq []
+    end
+
+    it 'is expected to be error if there is no keeper' do
+        expect{subject.borrow({})}.to  raise_error ('Required keeper')
+    end
+
+    it 'is expected to be error if there is no list of books' do
+        expect{subject.borrow({keeper: keeper})}.to raise_error ('Requred list of books')
+    end
+
+
+    
 
 
     
