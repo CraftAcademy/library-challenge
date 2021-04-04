c66b02174f544703c1bf9f52061a76de754fd286
kim_haaga <kim.haaga@live.se>
2021-04-04 16:36:19 +0200

    Add rent_the_book and give_receipt methods

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,12 +1,28 @@
-require 'yaml'
 require 'pry'
 require './lib/library.rb'
 
 class Visitor < Library    
+    attr_accessor :receipt
 
     def see_available                     
         all_books = self.read_book_list
         availible_books = all_books.select { |book| book[:available] == true }             
     end
 
+    def rent_the_book(args={})
+        self.pull_book(args)
+        @pulled_book[:available] == true ? give_receipt : book_unavailable
+    end
+    
+    private
+    
+    def book_unavailable
+        raise 'Book is unavailable'
+    end
+    
+    def give_receipt
+        self.change_status
+        self.save_book_list
+        @receipt = {title: @pulled_book[:item][:title], return_date: @pulled_book[:return_date], date: Date.today.strftime('%d-%m-%y') }
+    end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -14,7 +14,27 @@ describe Visitor do
         ]
         expect(subject.see_available).to eq expected_outcome
     end
-    
+   
+    it 'can rent the book' do
+        subject.read_book_list
+        subject.rent_the_book({title: 'Lord of the Rings'})
+        subject.read_book_list
+        subject.pull_book({title: 'Lord of the Rings'})
+        expected_outcome = {:item=>{:title=>"Lord of the Rings", :author=>"J.R.R. Tolkien"}, :available=>false, :return_date=>subject.thirty_days_later}
+        expect(subject.pulled_book).to eq expected_outcome
+    end
+
+    it 'sends a recipet' do
+        subject.read_book_list
+        subject.rent_the_book({title: 'Fifty Shades of Grey'})
+        expected_outcome = {title: "Fifty Shades of Grey", return_date: subject.thirty_days_later, date: Date.today.strftime('%d-%m-%y') }
+        expect(subject.receipt).to eq expected_outcome
+    end  
+
+    after(:all) do
+        book_list = YAML.load_file('./lib/books_original_state.yml')
+        File.open('./lib/books.yml', 'w') { |file| file.write book_list.to_yaml }
+      end
     
     #rent_the_book
     #see_currently_owned
