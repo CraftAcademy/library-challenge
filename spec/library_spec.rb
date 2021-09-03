fb366290e001af4a134d980a5261a361f9d0d1cc
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-03 22:00:26 +0200

    Added first test to library_spec.rb but test is going red

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,8 +1,13 @@
-require_relative "./lib/library"
+require_relative "../lib/library.rb"
+require 'date'
 
 describe Library do
   let(:objects) { instance_double()}
-end
 
-it "is expected to have a person class" do
+  collection = YAML.load_file('./lib/data.yml')
+  
+  it "is expected to show a list of books" do
+    expect(subject.collection).to_not eq nil 
+  end
 end
+  
