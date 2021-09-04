ded0143f90ea4fcebfb4a1c10a19f638738acc76
Fraser <Fraser.Hughes@btinternet.com>
2021-09-04 12:40:28 +0200

    Added to correct return statement to checkout method.

--- lib/library.rb
+++ lib/library.rb
@@ -6,21 +6,24 @@ class Library
   def initialize
     @list_of_books = YAML.load_file('./lib/base_data.yml')
     @updated_list = YAML.load_file('./lib/test_data.yml')
-
   end
 
   def search_for_title(title)
-    list_of_books.detect { |obj| obj[:item][:title].include? title }
+    list_of_books.detect { |obj| obj[:book][:title].include? title }
   end
 
   def search_for_author(author)
-    list_of_books.detect { |obj| obj[:item][:author] == author }
+    list_of_books.detect { |obj| obj[:book][:author] == author }
   end
 
   def checkout_book(title)
-    updated_list[0][:available] = false
+    updated_list[1][:available] = false
     File.open('./lib/test_data.yml', 'w') { |f| f.write updated_list.to_yaml }
-    
-  
+    updated_list.detect { |obj| obj[:book][:title].include? title }
   end
+
+private
+
+
+
 end
