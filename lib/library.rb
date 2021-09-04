f5b1bcd618aa72e8e13c3a965e29a2d9f4484758
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-04 12:26:14 +0200

    added test for book_return_date method. fails but think i need to write logic for the checking out books first

--- lib/library.rb
+++ lib/library.rb
@@ -1,19 +1,24 @@
 # date.next_month can be used to
 # put 30 days for books to be returned
-require "yaml"
+require 'yaml'
+require 'date'
 
 class Library
-  attr_accessor :collection, :item, :available
+  attr_accessor :collection, :return_date
 
-  def initialize(attrs = {})
-    @collection = YAML.load_file("./lib/data.yml")
+  def initialize(_attrs = {})
+    @collection = YAML.load_file('./lib/data.yml')
   end
 
-  def search(book)
+  def search(_book)
     collection.select { |book| book[:item][:title] == book }
   end
 
   def available_books
     @collection.select { |book| book[:available] == true }
+  end
+
+  def book_return_date
+    Date.today.next_month
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,24 +1,35 @@
-require_relative "../lib/library.rb"
-require "date"
+require_relative '../lib/library'
+require 'date'
+require 'pry'
 
 describe Library do
+  after do
+    # YAML source info https://medium.com/launch-school/storing-objects-in-yaml-with-ruby-d02c60e0dc65
+    test_data = YAML.load_file('./lib/data_test.yml')
+    File.open('./lib/data.yml', 'w') { |file| file.write test_data.to_yaml }
+  end
+
   # let(:objects) { instance_double() }
 
-  subject { described_class.new }
+  # subject { described_class.new }
 
-  it "is expected to show a list of books" do
+  it 'is expected to show a list of books' do
     expect(subject.collection).to_not eq nil
   end
 
-  it "is expected to search for a specific book by title or author" do
-    expected_output = YAML.load_file("./lib/data.yml").select { |book| book[:item][:title] == book }
-    expect(subject.search("Alfons")).to eq expected_output
+  it 'is expected to search for a specific book by title or author' do
+    expected_output = YAML.load_file('./lib/data.yml').select { |book| book[:item][:title] == book }
+    expect(subject.search('Alfons')).to eq expected_output
   end
 
-  it "it is expected to search for any available book by title or author" do
-    expected_output = YAML.load_file("./lib/data.yml").select { |book| book[:available] == true }
+  it 'it is expected to search for any available book by title or author' do
+    expected_output = YAML.load_file('./lib/data.yml').select { |book| book[:available] == true }
     expect(subject.available_books).to eq expected_output
   end
 
-
+  it 'is expected to have a return date within 30 days' do
+    book_return_date = YAML.load_file('./lib/data.yml').select { |book| book[:return_date] == Date.today.next_month }
+    # binding.pry
+    expect(subject.book_return_date).to be < :return_date
+  end
 end
