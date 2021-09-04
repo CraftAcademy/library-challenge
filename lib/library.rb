e06167ec215d2cd20017dca08f9104a0bd06cc01
serge_kamya <sergekamya@gmail.com>
2021-09-04 12:15:29 +0200

    created test and method for checkout_book

--- lib/library.rb
+++ lib/library.rb
@@ -1,17 +1,26 @@
 require 'yaml'
 
 class Library
-  attr_accessor :title, :list_of_books
+  attr_accessor :list_of_books, :updated_list
 
   def initialize
     @list_of_books = YAML.load_file('./lib/base_data.yml')
+    @updated_list = YAML.load_file('./lib/test_data.yml')
+
   end
 
   def search_for_title(title)
-    list_of_books.select { |obj| obj[:item][:title].include? title }
+    list_of_books.detect { |obj| obj[:item][:title].include? title }
   end
 
   def search_for_author(author)
     list_of_books.detect { |obj| obj[:item][:author] == author }
+  end
+
+  def checkout_book(title)
+    updated_list[0][:available] = false
+    File.open('./lib/test_data.yml', 'w') { |f| f.write updated_list.to_yaml }
+    
+  
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,13 +5,20 @@ describe Library do
   subject { described_class.new }
   it 'is expected to be able to search for books by title' do
     expect(subject.search_for_title('The Expanse')).to eq item: { author: 'James S. A. Corey',
-                                                                  title: 'The Expanse' }, available: true, 
-                                                                  return_date: nil
+                                                                  title: 'The Expanse' }, available: true,
+                                                          return_date: nil
   end
 
   it 'is expected tobe able to search for books by author' do
     expect(subject.search_for_author('James S. A. Corey')).to eq item: { author: 'James S. A. Corey',
-                                                                  title: 'The Expanse' }, available: true, 
-                                                                  return_date: nil
+                                                                         title: 'The Expanse' }, available: true,
+                                                                 return_date: nil
+  end
+
+  it 'it expected that a customer can check out a book' do
+    expect(subject.checkout_book('The Expanse')).to eq item: { author: 'James S. A. Corey',
+                                                                title: 'The Expanse' }, available: false,
+                                                      return_date: nil
   end
 end
+
