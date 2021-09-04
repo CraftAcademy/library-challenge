f7f784907bea8b365554f32128e9afb5ebf738be
maxarvid <max.arvid.anderson@gmail.com>
2021-09-04 12:45:58 +0200

    reworks availble_books and implements checkout method in Library class

--- lib/library.rb
+++ lib/library.rb
@@ -1,11 +1,24 @@
 require 'yaml'
+require 'date'
 
-class Library 
+class Library
+  attr_accessor :available_books
 
-
-  def available_books
-    collection = YAML.load_file('./lib/data.yml')
-    collection
+  def initialize
+    # maybe change into ALL Books and create available books with private method below.
+    @collection = YAML.load_file('./lib/data.yml')
+    @available_books = book_available
   end
 
+  def checkout(title)
+    successful_checkout = { status: true, message: 'Enjoy', date: Date.today, exp_date: Date.today.next_month }
+    unsuccessful_checkout = { status: false, message: 'Not available', date: Date.today }
+    available_books.any? { |book| book[:book][:title] == title } ? successful_checkout : unsuccessful_checkout
+  end
+
+  private
+
+  def book_available
+    @collection.select { |book| book[:available] = true }
+  end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,16 +1,15 @@
 require './lib/library'
+require 'date'
 
 describe Library do
+  it 'is expected to contain books' do
+    expect(subject.available_books).to be_truthy
+  end
 
-    it 'is expected to contain books' do
-        expect(subject.available_books).to be_truthy
-    end
-
-    it 'is expected to check out books' do
-        title = 'Alfons och soldatpappan'
-        expect_output = {status: true, message: 'Enjoy', date: Date.today, exp_date: Date.today + 1.months}
-        expect(subject.checkout(title)).to eq expect_output
-    end
-
-  
+  it 'is expected to check out books' do
+    title = 'Alfons och soldatpappan'
+    expected_output = { status: true, message: 'Enjoy', date: Date.today, exp_date: Date.today.next_month }
+    #binding.pry
+    expect(subject.checkout(title)).to eq expected_output
+  end
 end
