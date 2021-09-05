45facfe1e6ccaaf3ed779551f01e4cbdfe02fd89
serge_kamya <sergekamya@gmail.com>
2021-09-05 14:32:13 +0200

    refactoring. added expected_output variable to specs in library_spec.rb

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -14,9 +14,9 @@ describe Library do
   subject { described_class.new }
 
   it 'is expected to be able to search for books by title' do
-    expect(subject.search_for_title('The Expanse')).to eq [book: { author: 'James S. A. Corey',
-                                                                   title: 'The Expanse' }, available: true,
-                                                           return_date: nil, checked_out_by: nil]
+    expected_output = [{ book: { author: 'James S. A. Corey', title: 'The Expanse' }, available: true,
+                         return_date: nil, checked_out_by: nil }]
+    expect(subject.search_for_title('The Expanse')).to eq expected_output
   end
 
   # it 'is expected to return a message saying the book is already checked out' do
@@ -24,35 +24,40 @@ describe Library do
   # end
 
   it 'is expected to be able to search for books by author' do
-    expect(subject.search_for_author('James S. A. Corey')).to eq [book: { author: 'James S. A. Corey',
-                                                                          title: 'The Expanse' }, available: true,
-                                                                  return_date: nil,  checked_out_by: nil]
+    expected_output = [{ book: { author: 'James S. A. Corey', title: 'The Expanse' }, available: true, return_date: nil,
+                         checked_out_by: nil }]
+    expect(subject.search_for_author('James S. A. Corey')).to eq expected_output
   end
 
   it 'is expected that a customer can checkout a book' do
-    expect(subject.checkout_book('The Way Of Kings', 'Fraser')).to eq "Book checked out, please return #{Date.today.next_month.strftime('%d/%m/%y.')}"
+    expected_output = "Book checked out, please return #{Date.today.next_month.strftime('%d/%m/%y.')}"
+    expect(subject.checkout_book('The Way Of Kings', 'Fraser')).to eq expected_output
   end
 
   it 'is expected that if a book is already checked out it should tell the user it is unavailable' do
-    expect(subject.checkout_book('Code', 'Fraser')).to eq 'Book unavailable.'
+    expected_output = 'Book unavailable.'
+    expect(subject.checkout_book('Code', 'Fraser')).to eq expected_output
   end
 
   it 'is expected that a book that is being checked out has a return date' do
-    expect(subject.checkout_book('The Expanse', 'Fraser')).to eq "Book checked out, please return #{Date.today.next_month.strftime('%d/%m/%y.')}"
+    expected_output = "Book checked out, please return #{Date.today.next_month.strftime('%d/%m/%y.')}"
+    expect(subject.checkout_book('The Expanse', 'Fraser')).to eq expected_output
   end
 
   it 'is expected that when a book is checked out a name will be stored' do
-    expect(subject.checkout_book('The Expanse', 'Fraser')).to eq [book: { author: 'James S. A. Corey',
-                                                                     title: 'The Expanse' }, available: true,
-                                                             return_date: nil, checked_out_by: 'Fraser']
+    expected_output = [{ book: { author: 'James S. A. Corey', title: 'The Expanse' }, available: true,
+                         return_date: nil, checked_out_by: 'Fraser' }]
+    expect(subject.checkout_book('The Expanse', 'Fraser')).to eq expected_output
   end
 
   it 'is expected that a customer can return a book' do
-    expect(subject.return_book('Oliver Twist')).to eq 'Book returned.'
+    expected_output = 'Book returned.' 
+    expect(subject.return_book('Oliver Twist')).to eq expected_output 
   end
 
   it 'is expected to be able to search for all available books' do
-    list_of_books = YAML.load_file('./lib/base_data.yml')
-    expect(subject.list_available_books).to eq list_of_books.select { |obj| obj[:available] == true }
+     list_of_books = YAML.load_file('./lib/base_data.yml')
+     expected_output = list_of_books.select { |obj| obj[:available] == true } 
+    expect(subject.list_available_books).to eq expected_output
   end
 end
