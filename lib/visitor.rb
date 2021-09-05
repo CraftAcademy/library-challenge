d3a7e50c051fc04a9700f681797533b3ffe53cfc
Dorian Buck <[dorianbuck@gmail.com]>
2021-09-05 11:52:31 +0200

    moved some test to visitor perspective. 1 passing of 3

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -2,9 +2,29 @@ require './lib/library.rb'
 require 'yaml'
 
 class Visitor
-  attr_accessor :visitor_available_books
+  attr_accessor :visitor_available_books, :collection
 
   def visitor_available_books
-    library.available_books
+    YAML.load_file('./lib/data.yml').select { |book| book[:available] == true }
+  end
+
+  def available_books
+    @collection.select { |book| book[:available] == true }
+  end
+
+  def book_return_date
+    Date.today.next_month
+  end
+
+  def checkout_book(title)
+    book = @collection.detect { |book| book[:item][:title] == title }
+    # binding.pry
+    if book[:available] == true
+      book[:available] = false
+      book[:return_date] = book_return_date
+      return { message: 'Checkout of book success! Please return by:', date: Date.today.next_month }
+
+    end
+    File.open('./lib/data.yml', 'w') { |file| file.write collection.to_yaml }
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,17 +1,12 @@
 require_relative '../lib/library'
 require 'date'
 
-
 describe Library do
   after do
     # YAML source info https://medium.com/launch-school/storing-objects-in-yaml-with-ruby-d02c60e0dc65
     test_data = YAML.load_file('./lib/data_unchanged.yml')
     File.open('./lib/data.yml', 'w') { |file| file.write test_data.to_yaml }
   end
-
-  # let(:objects) { instance_double() }
-
-  # subject { described_class.new }
 
   it 'is expected to show a list of books' do
     expect(subject.collection).to_not eq nil
@@ -28,14 +23,14 @@ describe Library do
     expect(subject.available_books).to eq expected_output
   end
 
-  it 'is expected to checkout a book succesfully' do
-    expected_output = { message: 'Checkout of book success! Please return by:', date: Date.today.next_month }
-    # binding.pry
-    expect(subject.checkout_book('Star Trek')).to eq expected_output
-  end
+  # it 'is expected to checkout a book successfully' do
+  #   expected_output = { message: 'Checkout of book success! Please return by:', date: Date.today.next_month }
+  #   # binding.pry
+  #   expect(subject.checkout_book('Star Trek')).to eq expected_output
+  # end
 
-  it 'is expected to display a return date within 30 days of checkout' do
-    # binding.pry
-    expect(subject.checkout_book('The Prometheus Design')).to include(:date)
-  end
+  # it 'is expected to display a return date within 30 days of checkout' do
+  #   # binding.pry
+  #   expect(subject.checkout_book('The Prometheus Design')).to include(:date)
+  # end
 end
