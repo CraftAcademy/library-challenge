11297c7031844e390e7c19c5617fde478bb9f4c4
serge_kamya <sergekamya@gmail.com>
2021-09-05 17:41:41 +0200

    moved spec and method for list_available_books to from library.rb to person.rb file

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,7 +1,20 @@
+require "./lib/library"
+# require "yaml"
+# require "date"
+
 class Visitor
-  attr_accessor :name
+  attr_accessor :name, :list_of_books
 
   def initialize(name)
+    @list_of_books = YAML.load_file('./lib/base_data.yml')
     @name = name
   end
+
+
+  def list_available_books
+    list_of_books.select { |obj| obj[:available] == true }
+  end
+
+
 end
+
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,11 +2,15 @@ require './lib/visitor'
 require './lib/library'
 
 describe Visitor do
-  subject {described_class.new(name: 'Serge')}
-
+  subject { described_class.new(name: 'Serge') }
 
   it 'is expected to have a name on initialize' do
     expect(subject.name).not_to be nil
   end
 
+  it 'is expected to be able to search for all available books' do
+    list_of_books = YAML.load_file('./lib/base_data.yml')
+    expected_output = list_of_books.select { |obj| obj[:available] == true }
+    expect(subject.list_available_books).to eq expected_output
+  end
 end
