0cd5fe933c1f69b58a3722a28a458ae10e13c606
August <august.enhager@gmail.com>
2021-09-05 21:24:43 +0200

    adds implementation on name initialize

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,21 @@
 
 class Person
+    attr_accessor :name, :bookshelf
 
-    
+    def initialize(attrs = {})
+      set_name(attrs[:name])
+    end
+
+
+    private
+
+    def set_name(obj)
+      obj.nil? ? missing_name : @name = obj
+    end
+
+    def missing_name
+      raise 'A name is required'
+    end
+
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,14 +1,15 @@
 require './lib/person'
 
 describe Person do
-
+    subject { described_class.new(name: 'Thomas') }
+    
     it 'is expected to have a name initilazed' do
         expect(subject.name).not_to be nil
     end
 
-    it 'is expected to be able to checkout books' do
+    # it 'is expected to be able to checkout books' do
         
-    end
+    # end
 
     
   
