b9fbad6ed2682040af96ff8c89d5baac5a0c87d3
giacoletti <giacoletti92@gmail.com>
2021-11-13 22:29:04 +0100

    Adding library_spec.rb file, adding basic unit test structure WIP

---
+++ spec/library_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/library.rb"
+
+describe Library do
+  let(:book) { instance_double("Book", title: "Animal Farm", author: "George Orwell") }
+
+  describe "can add a book in the inventory" do
+    
+  end
+end
