0090108c0f19df7aa67071d1ee1f4625f4cbca53
DefyCab <love.bellinder@gmail.com>
2021-11-14 23:58:42 +0100

    format code with rufo

--- lib/library.rb
+++ lib/library.rb
@@ -1,21 +1,22 @@
 require "yaml"
 
 class Library
+  attr_accessor :books
 
-    attr_accessor :books
+  def initialize
+    @books = YAML.load_file("./lib/data.yml")
+  end
 
-    def initialize 
-        @books = YAML.load_file('./lib/data.yml')
-    end
+  def search_book_title
+  end
 
+  def search_book_author
+    books.select { |obj| obj[:book][:author].include? "Moberg" }
+  end
 
-    def search_book_title
-    end
+  def checkout_book
+  end
 
-    def search_book_author
-    books.select { |obj| obj[:book][:author].include? "Moberg"  }
-    end
-
-    def checkout_book
-    end
-end   
+  def search_result
+  end
+end
